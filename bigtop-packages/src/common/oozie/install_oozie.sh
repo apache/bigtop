55426cd95bc953c1caa7361aa744b459ac45af41
Roman Shaposhnik <rvs@apache.org>
2012-11-21 08:12:09 -0800

    BIGTOP-791. Bump Oozie to 3.3.0

--- bigtop-packages/src/common/oozie/install_oozie.sh
+++ bigtop-packages/src/common/oozie/install_oozie.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
+set -x
 
 # Licensed to the Apache Software Foundation (ASF) under one or more
 # contributor license agreements.  See the NOTICE file distributed with
@@ -15,12 +16,6 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 #
-
-failIfNotOK() {
-  if [ $? != 0 ]; then
-    exit $?
-  fi
-}
 
 usage() {
   echo "
@@ -107,37 +102,22 @@ DOC_DIR=${DOC_DIR:-$CLIENT_PREFIX/usr/share/doc/oozie}
 BIN_DIR=${CLIENT_PREFIX}/usr/bin
 
 install -d -m 0755 ${CLIENT_LIB_DIR}
-failIfNotOK
 install -d -m 0755 ${CLIENT_LIB_DIR}/bin
-failIfNotOK
 cp -R ${BUILD_DIR}/bin/oozie ${CLIENT_LIB_DIR}/bin
-failIfNotOK
 cp -R ${BUILD_DIR}/lib ${CLIENT_LIB_DIR}
-failIfNotOK
 install -d -m 0755 ${DOC_DIR}
-failIfNotOK
 cp -R ${BUILD_DIR}/LICENSE.txt ${DOC_DIR}
-failIfNotOK
 cp -R ${BUILD_DIR}/NOTICE.txt ${DOC_DIR}
-failIfNotOK
 cp -R ${BUILD_DIR}/oozie-examples.tar.gz ${DOC_DIR}
-failIfNotOK
 cp -R ${BUILD_DIR}/README.txt ${DOC_DIR}
-failIfNotOK
 cp -R ${BUILD_DIR}/release-log.txt ${DOC_DIR}
-failIfNotOK
 [ -f ${BUILD_DIR}/PATCH.txt ] && cp ${BUILD_DIR}/PATCH.txt ${DOC_DIR}
-# failIfNotOK
 cp -R ${BUILD_DIR}/docs/* ${DOC_DIR}
-failIfNotOK
 install -d -m 0755 ${MAN_DIR}
-failIfNotOK
 gzip -c ${EXTRA_DIR}/oozie.1 > ${MAN_DIR}/oozie.1.gz
-failIfNotOK
 
 # Create the /usr/bin/oozie wrapper
 install -d -m 0755 $BIN_DIR
-failIfNotOK
 cat > ${BIN_DIR}/oozie <<EOF
 #!/bin/sh
 #
@@ -165,9 +145,7 @@ fi
 
 exec /usr/lib/oozie/bin/oozie "\$@"
 EOF
-failIfNotOK
 chmod 755 ${BIN_DIR}/oozie
-failIfNotOK
 
 
 ## Install server image
@@ -176,67 +154,54 @@ CONF_DIR=${CONF_DIR:-"${SERVER_PREFIX}/etc/oozie/conf.dist"}
 DATA_DIR=${SERVER_PREFIX}/var/lib/oozie
 
 install -d -m 0755 ${SERVER_LIB_DIR}
-failIfNotOK
 install -d -m 0755 ${SERVER_LIB_DIR}/bin
-failIfNotOK
 install -d -m 0755 ${DATA_DIR}
-failIfNotOK
 for file in ooziedb.sh oozied.sh oozie-sys.sh ; do
   cp ${BUILD_DIR}/bin/$file ${SERVER_LIB_DIR}/bin
-  failIfNotOK
 done
-cp -R ${BUILD_DIR}/libtools ${SERVER_LIB_DIR}
-failIfNotOK
 
 install -d -m 0755 ${CONF_DIR}
-failIfNotOK
 cp ${BUILD_DIR}/conf/* ${CONF_DIR}
 sed -i -e '/oozie.service.HadoopAccessorService.hadoop.configurations/,/<\/property>/s#<value>\*=hadoop-conf</value>#<value>*=/etc/hadoop/conf</value>#g' \
           ${CONF_DIR}/oozie-site.xml
-failIfNotOK
+# FIXME: OOZIE-1089, MAPREDUCE-4820
+sed -i -e '/<\/configuration>/i<property><name>oozie.hadoop-2.0.2-alpha.workaround.for.distributed.cache</name><value>true</value></property>' \
+          ${CONF_DIR}/oozie-site.xml
 cp ${EXTRA_DIR}/oozie-env.sh ${CONF_DIR}
-failIfNotOK
 install -d -m 0755 ${CONF_DIR}/action-conf
-failIfNotOK
 cp ${EXTRA_DIR}/hive.xml ${CONF_DIR}/action-conf
-failIfNotOK
 if [ "${INITD_DIR}" != "" ]; then
   install -d -m 0755 ${INITD_DIR}
-  failIfNotOK
   cp -R ${EXTRA_DIR}/oozie.init ${INITD_DIR}/oozie
-  failIfNotOK
   chmod 755 ${INITD_DIR}/oozie
- failIfNotOK
 fi
 cp -R ${BUILD_DIR}/oozie-sharelib*.tar.gz ${SERVER_LIB_DIR}/oozie-sharelib.tar.gz
-failIfNotOK
 cp -R ${BUILD_DIR}/oozie-server/webapps ${SERVER_LIB_DIR}/webapps
-failIfNotOK
 ln -s -f /etc/oozie/conf/oozie-env.sh ${SERVER_LIB_DIR}/bin
-failIfNotOK
 
 # Unpack oozie.war some place reasonable
 WEBAPP_DIR=${SERVER_LIB_DIR}/webapps/oozie
 mkdir ${WEBAPP_DIR}
-failIfNotOK
 (cd ${WEBAPP_DIR} ; jar xf ${BUILD_DIR}/oozie.war)
-failIfNotOK
 mv -f ${WEBAPP_DIR}/WEB-INF/lib ${SERVER_LIB_DIR}/libserver
-failIfNotOK
 touch ${SERVER_LIB_DIR}/webapps/oozie.war
-failIfNotOK
+
+# Create all the jars needed for tools execution
+install -d -m 0755 ${SERVER_LIB_DIR}/libtools
+for i in `cd ${BUILD_DIR}/libtools ; ls *` ; do
+  if [ -e ${SERVER_LIB_DIR}/libserver/$i ] ; then
+    ln -s ../libserver/$i ${SERVER_LIB_DIR}/libtools/$i
+  else
+    cp ${BUILD_DIR}/libtools/$i ${SERVER_LIB_DIR}/libtools/$i
+  fi
+done
 
 # Create an exploded-war oozie deployment in /var/lib/oozie
 install -d -m 0755 ${SERVER_LIB_DIR}/oozie-server
-failIfNotOK
 cp -R ${BUILD_DIR}/oozie-server/conf ${SERVER_LIB_DIR}/oozie-server/conf
-failIfNotOK
 cp ${EXTRA_DIR}/context.xml ${SERVER_LIB_DIR}/oozie-server/conf/
-failIfNotOK
 cp ${EXTRA_DIR}/catalina.properties ${SERVER_LIB_DIR}/oozie-server/conf/
-failIfNotOK
 ln -s ../webapps ${SERVER_LIB_DIR}/oozie-server/webapps
-failIfNotOK
 
 # Provide a convenience symlink to be more consistent with tarball deployment
 ln -s ${DATA_DIR#${SERVER_PREFIX}} ${SERVER_LIB_DIR}/libext
