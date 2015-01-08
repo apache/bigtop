4da87e1d312d5a0f841e9a348a30763b5fb99759
evans_ye <innovansye@gmail.com>
2015-01-09 00:34:09 +0800

    BIGTOP-1508. fix Puppet warnings under Puppet 3

--- bigtop-deploy/puppet/manifests/site.pp
+++ bigtop-deploy/puppet/manifests/site.pp
@@ -67,3 +67,10 @@ node default {
 }
 
 Yumrepo<||> -> Package<||>
+
+if versioncmp($::puppetversion,'3.6.1') >= 0 {
+  $allow_virtual_packages = hiera('allow_virtual_packages',false)
+  Package {
+    allow_virtual => $allow_virtual_packages,
+  }
+}
