b6945922c6cd57bbe569676500b20fa44bd29105
Hadoop testing team <control-plane@altiscale.com>
2015-03-31 10:52:01 +0000

    added comments to the java files in the interfaces module

--- bigtop-tests/test-artifacts/interfaces/src/main/groovy/org/apache/bigtop/itest/interfaces/LongRunTests.java
+++ bigtop-tests/test-artifacts/interfaces/src/main/groovy/org/apache/bigtop/itest/interfaces/LongRunTests.java
@@ -1,5 +1,18 @@
 /**
  * Marker interface for fast-running tests.
+ *
+ * Priority of Test Categories is in the following order
+ * EssentialTests > NormalTests > LongRunTests
+ * Tests are categorized at both method and class level. A category when defined
+ * at class level means all the test methods in that test class fall under that
+ * category. To avoid rerunning a test method which falls under a category at
+ * method level and under another at class level, we have used <exclude.groups>
+ * in test-runtime/pom.xml. When a certain category is excluded all the tests
+ * which fall under that category are not executed.
+ * "A category defined at class level can be superseded only by a higher priority
+ * category in the method level"
+ * In case of NormalTests category, EssentialTests is excluded because in case of
+ * conflicts the later is defined at method level.
  */
 
 package org.apache.bigtop.itest.interfaces;
