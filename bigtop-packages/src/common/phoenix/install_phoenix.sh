db0c8e3c85df18988f594c8f65308a28c059ebe0
YoungWoo Kim <ywkim@apache.org>
2015-07-28 15:11:25 +0900

    BIGTOP-1941. Upgrade Phoenix to 4.4.0

--- bigtop-packages/src/common/phoenix/install_phoenix.sh
+++ bigtop-packages/src/common/phoenix/install_phoenix.sh
@@ -111,13 +111,13 @@ install -d -m 0755 $PREFIX/$MAN_DIR
 install -d -m 0755 $PREFIX/$ETC_DIR
 install -d -m 0755 $PREFIX/$CONF_DIR
 
-cp -ra $BUILD_DIR/lib/* $PREFIX/$LIB_DIR/lib
-cp -a $BUILD_DIR/phoenix*.jar $PREFIX/$LIB_DIR
-cp -a $BUILD_DIR/examples $PREFIX/$DOC_DIR
-cp -a  $BUILD_DIR/bin/*.txt $PREFIX/$DOC_DIR
+install -d -m 0755 $PREFIX/var/lib/phoenix
+install -d -m 0755 $PREFIX/var/log/phoenix
 
-cp -a $BUILD_DIR/bin/*.py $PREFIX/$BIN_DIR
-cp -a $BUILD_DIR/bin/*.properties $PREFIX/$BIN_DIR
+cp -ra $BUILD_DIR/{bin,lib} $PREFIX/$LIB_DIR/
+chmod 755 $PREFIX/$BIN_DIR/*.py
+cp -a $BUILD_DIR/*.jar $PREFIX/$LIB_DIR/
+cp -a $BUILD_DIR/examples $PREFIX/$DOC_DIR
 
 # Remove sources jar
 rm $PREFIX/$LIB_DIR/lib/phoenix-*-sources.jar
@@ -128,7 +128,10 @@ find $PREFIX/$LIB_DIR -name '*.jar' -exec chmod a-x {} \;
 # Create version independent symlinks
 
 # phoenix-client for clients like sqlline
-ln -s `cd $PREFIX/$LIB_DIR ; ls phoenix*-client.jar` $PREFIX/$LIB_DIR/phoenix-client.jar
+ln -s `cd $PREFIX/$LIB_DIR ; ls phoenix*-client.jar | grep -v thin` $PREFIX/$LIB_DIR/phoenix-client.jar
+
+# phoenix-thin-client
+ln -s `cd $PREFIX/$LIB_DIR ; ls phoenix*-thin-client.jar` $PREFIX/$LIB_DIR/phoenix-thin-client.jar
 
 # phoenix-server for placing on the HBase regionserver classpath
 ln -s `cd $PREFIX/$LIB_DIR ; ls phoenix*-server.jar` $PREFIX/$LIB_DIR/phoenix-server.jar
