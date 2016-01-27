06605712ea78fffa0d55679eb095efc6db89f091
Raj Desai <rddesai@us.ibm.com>
2016-01-27 14:25:23 -0800

    Initial checkin for native tests.

--- bigtop-tests/spec-tests/runtime/src/test/resources/testRuntimeSpecConf.groovy
+++ bigtop-tests/spec-tests/runtime/src/test/resources/testRuntimeSpecConf.groovy
@@ -119,16 +119,16 @@ specs {
         relative = true
       }
     }
-    'HADOOP_EJH2_HADOOP' {
-      name = 'HADOOP_EJH2_HADOOP'
+    'HADOOP_EJH2_1' {
+      name = 'HADOOP_EJH2'
       type = 'shell'
       arguments {
         command = '[ "${JAVA_HOME}xxx" != "xxx" ] || grep -E "^\\s*export\\s+JAVA_HOME=[\\w/]+" $HADOOP_CONF_DIR/hadoop-env.sh'
         message = 'JAVA_HOME is not set'
       }
     }
-    'HADOOP_EJH2_YARN' {
-      name = 'HADOOP_EJH2_YARN'
+    'HADOOP_EJH2_2' {
+      name = 'HADOOP_EJH2'
       type = 'shell'
       arguments {
         command = '[ "${JAVA_HOME}xxx" != "xxx" ] || grep -E "^\\s*export\\s+JAVA_HOME=[\\w/]+" $HADOOP_CONF_DIR/yarn-env.sh'
@@ -184,12 +184,52 @@ specs {
         referenceList = 'hadoop-yarn.list'
       }
     }
-    'HADOOP_GETCONF' {
-      name = 'HADOOP_GETCONF'
+    'HADOOP_CNATIVE1' {
+      name = 'HADOOP_CNATIVE1'
       type = 'shell'
       arguments {
-        command = '[ `${HADOOP_HDFS_HOME}/bin/hdfs getconf -confKey dfs.permissions.superusergroup` == "hadoop" ]'
-        message = 'It\' not possible to to determine key Hadoop configuration values by using ${HADOOP_HDFS_HOME}/bin/hdfs getconf'
+        command = 'hadoop checknative -a 2>/dev/null | grep hadoop | grep true'
+        message = 'hadoop-common-project must be build with -Pnative or -Pnative-win'
+      }
+    }
+    'HADOOP_CNATIVE2' {
+      name = 'HADOOP_CNATIVE2'
+      type = 'shell'
+      arguments {
+        command = 'hadoop checknative -a 2>/dev/null | grep snappy | grep true'
+        message = 'hadoop-common-project must be build with Prequire.snappy'
+      }
+    }
+    'HADOOP_HNATIVE1' {
+      name = 'HADOOP_HNATIVE1'
+      type = 'shell'
+      arguments {
+        command = 'hadoop checknative -a 2>/dev/null | grep hadoop | grep true'
+        message = 'hadoop-hdfs-project must be build with -Pnative or -Pnative-win'
+      }
+    }
+    'HADOOP_YNATIVE1' {
+      name = 'HADOOP_YNATIVE1'
+      type = 'shell'
+      arguments {
+        command = 'hadoop checknative -a 2>/dev/null | grep hadoop | grep true'
+        message = 'hadoop-yarn-project must be build with -Pnative or -Pnative-win'
+      }
+    }
+    'HADOOP_MNATIVE1' {
+      name = 'HADOOP_MNATIVE1'
+      type = 'shell'
+      arguments {
+        command = 'hadoop checknative -a 2>/dev/null | grep hadoop | grep true'
+        message = 'hadoop-mapreduce-project must be build with -Pnative or -Pnative-win'
+      }
+    }
+    'HADOOP_MNATIVE2' {
+      name = 'HADOOP_MNATIVE2'
+      type = 'shell'
+      arguments {
+        command = 'hadoop checknative -a 2>/dev/null | grep snappy | grep true'
+	message = 'hadoop-mapreduce-project must be build with Prequire.snappy'
       }
     }
   }
