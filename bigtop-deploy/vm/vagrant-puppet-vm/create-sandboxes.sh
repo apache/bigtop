d78693bed54fbb239aacff939a2e03663050d887
Konstantin Boudnik <cos@boudnik.org>
2016-03-03 11:33:16 -0800

    ODPI-72. Build out VM for sandbox/reference implementation
    (cherry picked from commit cb637b3419bedca944831d2bc5239813970e588b)

---
+++ bigtop-deploy/vm/vagrant-puppet-vm/create-sandboxes.sh
@@ -0,0 +1,29 @@
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#!/bin/bash
+
+set -ex
+WORKDIR=$0.$$.dir
+mkdir -p $WORKDIR
+VERSION=${1:-1.0}
+NAME="vagrant-odpi-v$VERSION"
+# clean everything
+vagrant destroy -f
+vagrant up
+vagrant package --output $WORKDIR/${NAME}.box
+vagrant halt
+vmname=`VBoxManage  list vms | cut -d \" -f 2`
+VBoxManage export $vmname --output=$WORKDIR/${NAME}.ova --ovf20
+vagrant destroy -f
