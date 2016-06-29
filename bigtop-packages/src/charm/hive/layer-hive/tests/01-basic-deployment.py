881e10a6af473538023232993a9f45a0904601f0
Kevin W Monroe <kevin.monroe@canonical.com>
2016-06-29 13:06:20 -0500

    update tests so we have a simple deployment along with a more complicated hadoop/hive deployment for smoke testing

--- bigtop-packages/src/charm/hive/layer-hive/tests/01-basic-deployment.py
+++ bigtop-packages/src/charm/hive/layer-hive/tests/01-basic-deployment.py
@@ -21,19 +21,17 @@ import amulet
 
 class TestDeploy(unittest.TestCase):
     """
-    Trivial deployment test for Apache Bigtop Hive.
-
-    This charm cannot do anything useful by itself, so integration testing
-    is done in the bundle.
+    Trivial deployment test for Apache Hive.
     """
+    @classmethod
+    def setUpClass(cls):
+        cls.d = amulet.Deployment(series='trusty')
+        cls.d.add('hive', 'hive')
+        cls.d.setup(timeout=900)
+        cls.d.sentry.wait(timeout=1800)
 
     def test_deploy(self):
-        self.d = amulet.Deployment(series='trusty')
-        self.d.add('hive', 'hive')
-        self.d.setup(timeout=900)
-        self.d.sentry.wait(timeout=1800)
-        self.unit = self.d.sentry['hive'][0]
-
+        self.d.sentry.wait_for_messages({"hive": "waiting on relation to java"})
 
 if __name__ == '__main__':
     unittest.main()
---
+++ bigtop-packages/src/charm/hive/layer-hive/tests/02-smoke-test.py
@@ -0,0 +1,70 @@
+#!/usr/bin/env python3
+
+# Licensed to the Apache Software Foundation (ASF) under one or more
+# contributor license agreements.  See the NOTICE file distributed with
+# this work for additional information regarding copyright ownership.
+# The ASF licenses this file to You under the Apache License, Version 2.0
+# (the "License"); you may not use this file except in compliance with
+# the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import amulet
+import re
+import unittest
+
+
+class TestDeploy(unittest.TestCase):
+    """
+    Hadoop/Hive deployment and smoke test for the Apache Bigtop Hive service.
+    """
+    @classmethod
+    def setUpClass(cls):
+        cls.d = amulet.Deployment(series='trusty')
+        cls.d.add('hive', 'hive')
+        cls.d.add('namenode', 'hadoop-namenode')
+        cls.d.add('openjdk', 'openjdk')
+        cls.d.add('plugin', 'hadoop-plugin')
+        cls.d.add('resourcemanager', 'hadoop-resourcemanager')
+        cls.d.add('slave', 'hadoop-slave')
+
+        cls.d.configure('openjdk', {'java-type': 'jdk',
+                                    'java-major': '8'})
+
+        cls.d.relate('hive:hadoop', 'plugin:hadoop-plugin')
+        cls.d.relate('plugin:namenode', 'namenode:namenode')
+        cls.d.relate('plugin:resourcemanager', 'resourcemanager:resourcemanager')
+        cls.d.relate('resourcemanager:namenode', 'namenode:namenode')
+        cls.d.relate('slave:namenode', 'namenode:datanode')
+        cls.d.relate('slave:resourcemanager', 'resourcemanager:nodemanager')
+
+        cls.d.relate('hive:java', 'openjdk:java')
+        cls.d.relate('plugin:java', 'openjdk:java')
+        cls.d.relate('namenode:java', 'openjdk:java')
+        cls.d.relate('resourcemanager:java', 'openjdk:java')
+        cls.d.relate('slave:java', 'openjdk:java')
+
+        cls.d.setup(timeout=1800)
+        cls.d.sentry.wait_for_messages({'hive': re.compile('ready')}, timeout=1800)
+        cls.hive = cls.d.sentry['hive'][0]
+
+    def test_hive(self):
+        """
+        Validate Hive by running the smoke-test action.
+        """
+        uuid = self.hive.action_do('smoke-test')
+        result = self.d.action_fetch(uuid)
+        # hive smoke-test sets outcome=success on success
+        if (result['outcome'] != "success"):
+            error = "Hive smoke-test failed"
+            amulet.raise_status(amulet.FAIL, msg=error)
+
+
+if __name__ == '__main__':
+    unittest.main()
