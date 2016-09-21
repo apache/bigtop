9b775291f71beda2f5463a46082ce6df38bf4514
Roman Shaposhnik <rvs@apache.org>
2016-09-20 17:06:06 -0700

    BIGTOP-2537. Hive smoke test identifies as org.apache.bigtop.itest.hadoop.mapreduce

--- bigtop-tests/smoke-tests/hive/TestHiveSimple.groovy
+++ bigtop-tests/smoke-tests/hive/TestHiveSimple.groovy
@@ -16,7 +16,7 @@
  * limitations under the License.
  */
 
-package org.apache.bigtop.itest.hadoop.mapreduce
+package org.apache.bigtop.itest.hive
 
 import org.junit.BeforeClass
 import org.junit.AfterClass
