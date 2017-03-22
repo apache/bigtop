599207ff8157b56e87f94623277fe345a3af57c7
Roman Shaposhnik <rvs@apache.org>
2017-03-22 14:58:00 -0700

    BIGTOP-2664. create provisioner test for trunk packages with jdk8

--- docker/sandbox/sandbox-env.sh
+++ docker/sandbox/sandbox-env.sh
@@ -18,7 +18,7 @@
 RPMS=(centos-6 centos-7 fedora-20)
 DEBS=(debian-8 ubuntu-14.04)
 
-RPM_JDK=java-1.7.0-openjdk-devel.x86_64
-DEB_JDK=openjdk-7-jdk
+RPM_JDK=java-1.8.0-openjdk-devel.x86_64
+DEB_JDK=openjdk-8-jdk
 
 BIGTOP_VERSION=1.1.0
