c44c68028dc77993b7b22ef0edba75f2874ce1da
Olaf Flebbe <of@oflebbe.de>
2017-06-07 20:23:08 +0200

    BIGTOP-2800: provisioner fails for kerberos on centos-7

--- bigtop-deploy/puppet/modules/kerberos/manifests/init.pp
+++ bigtop-deploy/puppet/modules/kerberos/manifests/init.pp
@@ -121,20 +121,23 @@ class kerberos {
 
 
     class admin_server inherits kerberos::kdc {
-      $se_hack = "setsebool -P kadmind_disable_trans  1 ; setsebool -P krb5kdc_disable_trans 1"
 
       package { "$package_name_admin":
         ensure => installed,
         require => Package["$package_name_kdc"],
-      } 
-  
+      }
+
+      exec { '/usr/bin/setsebool -P kadmind_disable_trans 1':
+        onlyif => '/usr/bin/test -f /usr/bin/setsebook'
+      } ->
+      exec { '/usr/bin/setsebool -P krb5kdc_disable_trans 1':
+        onlyif => '/usr/bin/test -f /usr/bin/setsebook'
+      } ->
       service { "$service_name_admin":
         ensure => running,
         require => [Package["$package_name_admin"], Service["$service_name_kdc"]],
         subscribe => [File["${kdc_etc_path}/kadm5.acl"], File["${kdc_etc_path}/kdc.conf"]],
         hasrestart => true,
-        restart => "${se_hack} ; service ${service_name_admin} restart",
-        start => "${se_hack} ; service ${service_name_admin} start",
       }
     }
   }
