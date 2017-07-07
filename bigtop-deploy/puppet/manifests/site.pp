40e796bd693a8bc6243ee39a6540357e1d559ea8
Kevin W Monroe <kevin.monroe@canonical.com>
2017-07-07 16:02:47 +0000

    BIGTOP-2835: puppet fails when bigtop::jdk_preinstalled is true
    Many modules require Package["jdk"]. Ensure it is available regardless
    of the bigtop::jdk_preinstalled option. Modify the jdk class so the
    package resources perform a no-op if bigtop::jdk_preinstalled is true.
    
    Signed-off-by: Evans Ye <evansye@apache.org>

--- bigtop-deploy/puppet/manifests/site.pp
+++ bigtop-deploy/puppet/manifests/site.pp
@@ -13,11 +13,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-$jdk_preinstalled = hiera("bigtop::jdk_preinstalled", false)
-if ( ! $jdk_preinstalled ) {
-   require jdk
-   Class['jdk'] -> Service<||>
-}
+require jdk
+Class['jdk'] -> Service<||>
 
 $provision_repo = hiera("bigtop::provision_repo", true)
 if ($provision_repo) {
