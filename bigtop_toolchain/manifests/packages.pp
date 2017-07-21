ef61e5721c31e0dc89b865a176298094e54401a2
Evans Ye <evansye@apache.org>
2017-07-21 18:48:13 +0000

    BIGTOP-2850. Not able to build bigtop/slaves image for OpenSuSE 42.1

--- bigtop_toolchain/manifests/packages.pp
+++ bigtop_toolchain/manifests/packages.pp
@@ -113,24 +113,12 @@ class bigtop_toolchain::packages {
         "libevent-devel",
         "bison",
         "flex",
-        "libffi48-devel"
+        "libffi48-devel",
+        "libapr1-devel"
       ]
       # fix package dependencies: BIGTOP-2120 and BIGTOP-2152 and BIGTOP-2471
-      exec { '/usr/bin/zypper -n install  --force-resolution krb5 libopenssl-devel':
+      exec { '/usr/bin/zypper -n install libopenssl-devel':
       } -> Package <| |>
-      # fix package libapr1
-      exec { 'suse_12.3_repo':
-        command => '/usr/bin/zypper ar --no-gpgcheck http://download.opensuse.org/distribution/12.3/repo/oss/suse/ libapr1',
-        unless => "/usr/bin/zypper lr | grep -q libapr1",
-      }
-      package { 'libapr1':
-        ensure => '1.4.6',
-        require => [Exec['suse_12.3_repo']]
-      }
-      package { 'libapr1-devel':
-        ensure => '1.4.6',
-        require => [Package['libapr1']]
-      }
     }
     /Amazon/: { $pkgs = [
       "unzip",
