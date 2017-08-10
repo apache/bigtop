04023e6a58599fdae3078be52c0576b50360c060
bharatm363 <30471227+bharatm363@users.noreply.github.com>
2017-08-10 13:12:28 -0400

    Updated Class
    Attempted to clean up the class from spaces, printlns and added the licesnse. I have kept the println's instead of adding logger because the assertEquals result is the only relevant external output.

--- bigtop-tests/smoke-tests/hive/TestMethods.java
+++ bigtop-tests/smoke-tests/hive/TestMethods.java
@@ -1,5 +1,16 @@
-
-//Author: Bharat Modi
+//Copyright [2017] [Bharat Modi]
+//
+//Licensed under the Apache License, Version 2.0 (the "License");
+//you may not use this file except in compliance with the License.
+//You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+//Unless required by applicable law or agreed to in writing, software
+//distributed under the License is distributed on an "AS IS" BASIS,
+//WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+//See the License for the specific language governing permissions and
+//limitations under the License.
 // A masterclass containing methods which aid in the replication of access to hadoop
 import java.io.BufferedInputStream;
 import java.io.File;
@@ -29,14 +40,18 @@ import org.w3c.dom.Element;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
 import org.xml.sax.SAXException;
+
 public class TestMethods {
 
-	protected static String propertyValue(String propertyFile, String propertyName) throws ParserConfigurationException, SAXException, IOException, URISyntaxException{
+	static String propertyValue(String propertyFile, String propertyName)
+			throws ParserConfigurationException, SAXException, IOException,
+			URISyntaxException {
 		String configLocation = System.getenv("HADOOP_CONF_DIR");
-		File file = new File(configLocation+"/"+propertyFile);
+		File file = new File(configLocation + "/" + propertyFile);
 		DocumentBuilderFactory documentBuilderFactory = DocumentBuilderFactory
 				.newInstance();
-		DocumentBuilder documentBuilder = documentBuilderFactory.newDocumentBuilder();
+		DocumentBuilder documentBuilder = documentBuilderFactory
+				.newDocumentBuilder();
 		Document document = documentBuilder.parse(file);
 		document.getDocumentElement().normalize();
 		Element docElement = document.getDocumentElement();
@@ -49,8 +64,10 @@ public class TestMethods {
 				if (nodeList.item(i).getNodeType() == Node.ELEMENT_NODE) {
 					Element element = (Element) nodeList.item(i);
 					if (element.getNodeName().contains("property")) {
-						names.add( element.getElementsByTagName("name").item(0).getTextContent());
-						values.add( element.getElementsByTagName("value").item(0).getTextContent());
+						names.add(element.getElementsByTagName("name").item(0)
+								.getTextContent());
+						values.add(element.getElementsByTagName("value")
+								.item(0).getTextContent());
 
 					}
 				}
@@ -63,57 +80,57 @@ public class TestMethods {
 		return propertyValue;
 	}
 
-
-	protected static void dropTable(Statement stmt, String newTableName) throws SQLException{
+	static void dropTable(Statement stmt, String newTableName)
+			throws SQLException {
 		stmt.executeUpdate("DROP TABLE IF EXISTS " + newTableName);
 	}
 
-	protected static void createTable(Statement stmt, String newTableName, String columnNames, String delimiter) throws SQLException{
-		stmt.execute("CREATE TABLE "
-				+ newTableName
-				+ " ("+columnNames+") ROW FORMAT DELIMITED FIELDS TERMINATED BY '"+delimiter+"'");
-		System.out.println("Creating Table "+ newTableName +"\n");
+	static void createTable(Statement stmt, String newTableName,
+			String columnNames, String delimiter) throws SQLException {
+		stmt.execute("CREATE TABLE " + newTableName + " (" + columnNames
+				+ ") ROW FORMAT DELIMITED FIELDS TERMINATED BY '" + delimiter
+				+ "'");
+		System.out.println("Creating Table " + newTableName + "\n");
 	}
 
-	protected static void describeTable(Statement stmt, String newTableName) throws SQLException{
+	static void describeTable(Statement stmt, String newTableName)
+			throws SQLException {
 		ResultSet res;
 		String sql = "describe " + newTableName;
 		System.out.println("Running: " + sql);
 		res = stmt.executeQuery(sql);
 		while (res.next()) {
 			System.out.println(res.getString(1) + "\t" + res.getString(2)
-					+ "\t" + res.getString(3)+ "\t");
+					+ "\t" + res.getString(3) + "\t");
 		}
 	}
 
-	protected static void showTables(Statement stmt, String sql) throws SQLException{
+	static void showTables(Statement stmt, String sql) throws SQLException {
 		ResultSet res;
-		System.out.println("Running: " + sql +"\n");
+		System.out.println("Running: " + sql + "\n");
 		res = stmt.executeQuery(sql);
 		ResultSetMetaData rsmd = res.getMetaData();
 		int columnsNumber = rsmd.getColumnCount();
-
 
 		while (res.next()) {
 
 			for (int i = 1; i <= columnsNumber; i++) {
 				String columnValue = res.getString(i);
-				System.out.print(columnValue);
-				System.out.println("");
+				System.out.println(columnValue);
 			}
 
 		}
 		System.out.println("");
 	}
 
-	protected static void loadFile(String localFilepath, String HdfsURI, String fileDestination) throws IllegalArgumentException, IOException, URISyntaxException{
+	static void loadFile(String localFilepath, String HdfsURI,
+			String fileDestination) throws IllegalArgumentException,
+			IOException, URISyntaxException {
 		Configuration conf = new Configuration();
 		InputStream inputStream = new BufferedInputStream(new FileInputStream(
 				localFilepath));
-		FileSystem hdfs = FileSystem.get(new URI(HdfsURI),
-				conf);
-		OutputStream outputStream = hdfs.create(new Path(
-				fileDestination));
+		FileSystem hdfs = FileSystem.get(new URI(HdfsURI), conf);
+		OutputStream outputStream = hdfs.create(new Path(fileDestination));
 		try {
 			IOUtils.copyBytes(inputStream, outputStream, 4096, false);
 		} finally {
@@ -123,18 +140,18 @@ public class TestMethods {
 
 	}
 
-	protected static void loadData(Statement stmt, String filePath, String newTableName) throws SQLException{
-		String sql = "LOAD data inpath '" + filePath + "' OVERWRITE into table "
-				+ newTableName;
-		System.out.println("Running: " + sql +"\n");
+	static void loadData(Statement stmt, String filePath, String newTableName)
+			throws SQLException {
+		String sql = "LOAD data inpath '" + filePath
+				+ "' OVERWRITE into table " + newTableName;
+		System.out.println("Running: " + sql + "\n");
 		stmt.executeUpdate(sql);
 	}
 
-
-	protected static void deleteFile(Statement stmt, Path upload, String HdfsURI) throws IOException, URISyntaxException{
+	static void deleteFile(Statement stmt, Path upload, String HdfsURI)
+			throws IOException, URISyntaxException {
 		Configuration conf = new Configuration();
-		FileSystem hdfs = FileSystem.get(new URI(HdfsURI),
-				conf);
+		FileSystem hdfs = FileSystem.get(new URI(HdfsURI), conf);
 		hdfs.getConf();
 		if (hdfs.exists(upload)) {
 			hdfs.delete(upload, true);
@@ -142,44 +159,42 @@ public class TestMethods {
 		}
 	}
 
-	protected static void updateTable(Statement stmt, String selection) throws SQLException {
-		String sql=selection;
-		int affectedRows= stmt.executeUpdate(sql);
-		System.out.println("Updating Table: "+sql+"\n");
-		System.out.println("Affected Rows: "+affectedRows);
+	static void updateTable(Statement stmt, String selection)
+			throws SQLException {
+		String sql = selection;
+		int affectedRows = stmt.executeUpdate(sql);
+		System.out.println("Updating Table: " + sql + "\n");
+		System.out.println("Affected Rows: " + affectedRows);
 	}
 
-	protected static String printResults(Statement stmt, String selection, int columnVerificationNumber) throws SQLException{
+	static String printResults(Statement stmt, String selection,
+			int columnVerificationNumber) throws SQLException {
 		ResultSet res;
-		String sql= selection;
-		res=stmt.executeQuery(sql);
-		System.out.println("\n"+"Printing Results: "+sql+"\n");
+		String sql = selection;
+		res = stmt.executeQuery(sql);
+		System.out.println("\n" + "Printing Results: " + sql + "\n");
 		ResultSetMetaData rsmd = res.getMetaData();
 		int columnsNumber = rsmd.getColumnCount();
-		for (int q=1; q<=columnsNumber; q++){
-			System.out.print(rsmd.getColumnName(q) +" ");
+		for (int q = 1; q <= columnsNumber; q++) {
+			System.out.print(rsmd.getColumnName(q) + " ");
 		}
-		System.out.println();
-		System.out.println();
+		System.out.println("\n");
 		while (res.next()) {
 			for (int i = 1; i <= columnsNumber; i++) {
 				String columnValue = res.getString(i);
-				System.out.print(columnValue +"  ");
+				System.out.print(columnValue + "  ");
 			}
 			System.out.println("");
 		}
-		//The following is deprecated
-		//		System.out.println("\n"+"Printing Results: First 5 Columns"+"\n");
-		//		while(res.next()){
-		//			System.out.println(res.getString(columnList[0])+"  "+res.getString(columnList[1])+" "+res.getString(columnList[2])+" "+res.getString(columnList[3])+" "+res.getString(columnList[4])+"\n");
-		//		}
-		res=stmt.executeQuery(sql);
+
+		res = stmt.executeQuery(sql);
 		return resultSetVerification(res, columnVerificationNumber);
 	}
 
-	protected static String resultSetVerification(ResultSet res, int columnVerificationNumber) throws SQLException{
+	static String resultSetVerification(ResultSet res,
+			int columnVerificationNumber) throws SQLException {
 		String validate = null;
-		while (res.next()){
+		while (res.next()) {
 
 			validate = res.getString(columnVerificationNumber);
 
@@ -187,8 +202,8 @@ public class TestMethods {
 		return validate;
 	}
 
-	protected static void executeStatement(Statement stmt, String sqlStatement) throws SQLException{
+	static void executeStatement(Statement stmt, String sqlStatement)
+			throws SQLException {
 		stmt.execute(sqlStatement);
 	}
 }
-
