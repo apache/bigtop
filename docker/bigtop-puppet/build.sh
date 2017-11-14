f3fc0770422027180665ad90512c9269e4841bba
Jun He <jun.he@linaro.org>
2017-11-14 17:17:13 +0800

    BIGTOP-2922: Drop arch specific bigtop-* images
    Use unified Dockerfile for puppet/slaves images on different architectures.
    Simplified the build process, and add gradle tasks for them.
    Example: ./gradlew -POS=debian-9 -Pprefix=1.2.1 bigtop-puppet
             ./gradlew -POS=debian-9 -Pprefix=1.2.1 bigtop-slaves
    This will create proper images with same image name as existings on different
    HW architectures. This eases efforts for deploy and test.
    
    Change-Id: I183452324faf54808a4e88981d604d8fe119b51a
    Signed-off-by: Jun He <jun.he@linaro.org>
    Signed-off-by: Evans Ye <evansye@apache.org>

--- docker/bigtop-puppet/build.sh
+++ docker/bigtop-puppet/build.sh
@@ -12,11 +12,38 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-FROM bigtop/puppet:trunk-ubuntu-16.04
-MAINTAINER Amir Sanjar
 
-COPY bigtop_toolchain /etc/puppet/modules/bigtop_toolchain
+#!/bin/sh
 
-RUN  apt-get clean && apt-get update && puppet apply -e "include bigtop_toolchain::installer"
-COPY . /tmp/bigtop
-RUN cd /tmp/bigtop && ./gradlew && cd && rm -rf /tmp/bigtop
+set -ex
+
+if [ $# != 1 ]; then
+  echo "Creates bigtop/puppet image"
+  echo
+  echo "Usage: build.sh <PREIX-OS-VERSION>"
+  echo
+  echo "Example: build.sh trunk-centos-7"
+  echo "       : build.sh 1.0.0-centos-7"
+  exit 1
+fi
+
+PREFIX=$(echo "$1" | cut -d '-' -f 1)
+OS=$(echo "$1" | cut -d '-' -f 2)
+VERSION=$(echo "$1" | cut -d '-' -f 3)
+ARCH=$(uname -m)
+if [ "${ARCH}" != "x86_64" ];then
+ARCH="-${ARCH}"
+else
+ARCH=""
+fi
+
+cp ../../bigtop_toolchain/bin/puppetize.sh .
+cat >Dockerfile <<EOF
+FROM ${OS}:${VERSION}
+MAINTAINER dev@bigtop.apache.org
+COPY puppetize.sh /tmp/puppetize.sh
+RUN bash /tmp/puppetize.sh
+EOF
+
+docker build -t bigtop/puppet:${PREFIX}-${OS}-${VERSION}${ARCH} .
+rm -f Dockerfile puppetize.sh
