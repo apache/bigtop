d1f718768d5b9d1b9968938762217a30fda9c733
bharatm363 <30471227+bharatm363@users.noreply.github.com>
2018-06-26 16:00:50 -0400

    Add files via upload

--- bigtop-tests/smoke-tests/hive/HiveJdbcGeneralTest.java
+++ bigtop-tests/smoke-tests/hive/HiveJdbcGeneralTest.java
@@ -22,20 +22,18 @@ import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
 
 import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.IOException;
-import java.net.URISyntaxException;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Properties;
 
-import javax.xml.parsers.ParserConfigurationException;
-
 import org.apache.hive.service.cli.HiveSQLException;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.junit.Test;
-import org.xml.sax.SAXException;
 
 /**
  *
@@ -50,26 +48,13 @@ import org.xml.sax.SAXException;
  */
 public class HiveJdbcGeneralTest extends TestMethods {
 
-  @Test // (expected=java.sql.SQLDataException.class)
-  public void testTableCreation()
-      throws SQLException, ClassNotFoundException, InstantiationException,
-      IllegalAccessException, IOException, URISyntaxException,
-      FileNotFoundException, ParserConfigurationException, SAXException {
-    final File f = new File(HiveJdbcGeneralTest.class.getProtectionDomain()
-        .getCodeSource().getLocation().getPath());
-    // String jdbcDriver = propertyValue("hive-site.xml",
-    // "javax.jdo.option.ConnectionDriverName");
-    String hivePort = System.getenv("HIVE_PORT");
-    // String qualifiedName = propertyValue("hdfs-site.xml",
-    // "dfs.internal.nameservices");
-    // String[] haNodes = propertyValue("hdfs-site.xml",
-    // "dfs.ha.namenodes."+qualifiedName).split(",");
-    // String primaryNode = haNodes[0];
-    String hdfsConnection =
-        propertyValue("hdfs-site.xml", "dfs.namenode.rpc-address");
-    String jdbcConnection = System.getenv("HIVE_JDBC_URL");
+  static String hivePort = System.getenv("HIVE_PORT");
+  static String jdbcConnection = System.getenv("HIVE_JDBC_URL");
+  static Connection con;
+  String newTableName = "btest";
 
-    Connection con;
+  @BeforeClass
+  public static void onTimeSetup() throws Exception {
     String username = System.getenv("HIVE_USER");
     String password = System.getenv("HIVE_PASSWORD");
     Properties connectionProps = new Properties();
@@ -78,91 +63,190 @@ public class HiveJdbcGeneralTest extends TestMethods {
     Class.forName("org.apache.hive.jdbc.HiveDriver");
     con = DriverManager.getConnection(
         jdbcConnection + ":" + hivePort + "/default;", connectionProps);
-    Statement stmt = con.createStatement();
-    String newTableName = "btest";
-    String columnNames =
-        "(Flight int, Dates varchar(255), Depart varchar(10), Orig varchar(5), Dest varchar(5), Miles int, Boarded int, Capacity int)";
-    String partitionedColumns =
-        "(Flight int, Dates varchar(255), Depart varchar(10), Orig varchar(5), Dest varchar(5), Miles int, Boarded int)";
-    String localFilepath = f + "/samdat1.csv";
-    String HdfsURI = "hdfs://" + hdfsConnection;
-    String filePath = "/tmp/htest/00000_";
-    String fileDestination = HdfsURI + filePath;
-    assertFalse(con.getMetaData().supportsRefCursors());
-    assertTrue(con.getMetaData().allTablesAreSelectable());
-    System.out
-    .println("Hive Version: " + con.getMetaData().getDatabaseMajorVersion()
-        + "." + con.getMetaData().getDatabaseMinorVersion());
-    getTables(con, newTableName);
-    dropTable(stmt, newTableName);
-    dropTable(stmt, newTableName + "NT");
-    dropTable(stmt, newTableName + "T");
-    dropTable(stmt, newTableName + "P");
-    showTables(stmt, "show tables like 'b*'");
-    createTable(stmt, newTableName, columnNames, ",", "");
-    try {
-      createTable(stmt, newTableName + "NT", columnNames, ",",
-          "TBLPROPERTIES(\"transactional\"=\"true\")");
-      fail("shouldn't get here");
-    } catch (SQLException e) {
+  }
 
-    }
-    createTable(stmt, newTableName + "T", columnNames, ",",
-        "STORED AS ORC TBLPROPERTIES(\"transactional\"=\"true\")");
-    createPartitionedTable(stmt, newTableName + "P", partitionedColumns,
-        "(Capacity int)", ",", "STORED AS ORC");
-    showTables(stmt, "show tables like 'b*'");
-    loadFile(localFilepath, HdfsURI, fileDestination + ".txt");
-    loadData(stmt, filePath + ".txt", newTableName);
-    describeTable(stmt, newTableName);
-    updateTable(stmt, "Insert into table btestt SELECT * from btest");
-    updateTable(stmt,
-        "Insert into table btestp PARTITION (Capacity) SELECT * from btest");
-    deleteFile(stmt, filePath + ".txt", HdfsURI);
-    deleteFile(stmt, filePath + "0.orc", HdfsURI);
-    assertEquals(printResults(stmt, "Select * from btest"), "302");
-    assertEquals(
-        printResults(stmt,
-            "Select * from btest where Dest = 'LAX' order by boarded desc"),
-        "114");
-    assertEquals(printResults(stmt,
-        "Select * from btest where boarded between 160 and 180 order by boarded asc"),
-        "622");
-    assertEquals(printResults(stmt,
-        "Select * from btest where Dest='LAX' or Dest='ORD'"), "202");
-    assertEquals(printResults(stmt,
-        "Select * from btest where Dest= 'LAX' and boarded >= 180"), "114");
-    assertEquals(printResults(stmt,
-        "Select * from btest where Dest ='LAX' and boarded = 197"), "114");
-    assertEquals(preparedStatement(con,
-        "Select * from btest where Dest ='LAX' and boarded = 197"), "114");
-    assertEquals(setFetchSizeStatement(stmt), 15);
-    assertEquals(setFetchSizePreparedStatement(con), 15);
-    // assertEquals(callableStatement(con, -20), -1);
-    assertEquals(printResults(stmt, "Select SUM(Miles) from btest"), "95487");
-    assertEquals(printResults(stmt, "Select * from btest"), "302");
-    assertEquals(
-        updateTable(stmt, "Update btestt set Orig= 'test' where Dest= 'LAX'"),
-        0);
-    try {
-      loadData(stmt, filePath + ".txt", newTableName + "T");
-      fail("shouldn't get here");
-    } catch (HiveSQLException e) {
-      System.out.println("File does not exist in specified location");
-    }
-    assertEquals(printResults(stmt, "Select * from btestt order by Dest"),
-        "132");
-    assertEquals(printResults(stmt, "show partitions btestp"), "capacity=250");
-    assertEquals(printResults(stmt,
-        "select MIN(Dest), boarded from btest where Dest ='LAX' group by Dest, boarded"),
-        "LAX");
-    printResults(stmt, "Describe formatted btest");
-    dropTable(stmt, newTableName);
-    dropTable(stmt, newTableName + "NT");
-    dropTable(stmt, newTableName + "T");
-    dropTable(stmt, newTableName + "P");
-    setNegativeFetchSize(stmt);
+  @AfterClass
+  public static void oneTimeTearDown() throws Exception {
     con.close();
   }
 
+  @Test // (expected=java.sql.SQLDataException.class)
+  public void testHive() throws Exception {
+    final File f = new File(HiveJdbcGeneralTest.class.getProtectionDomain()
+        .getCodeSource().getLocation().getPath());
+    // String jdbcDriver = propertyValue("hive-site.xml",
+    // "javax.jdo.option.ConnectionDriverName");
+
+    // String qualifiedName = propertyValue("hdfs-site.xml",
+    // "dfs.internal.nameservices");
+    // String[] haNodes = propertyValue("hdfs-site.xml",
+    // "dfs.ha.namenodes."+qualifiedName).split(",");
+    // String primaryNode = haNodes[0];
+    String hdfsConnection =
+        propertyValue("hdfs-site.xml", "dfs.namenode.rpc-address");
+    try (Statement stmt = con.createStatement()) {
+      String columnNames =
+          "(Flight int, Dates varchar(255), Depart varchar(10), Orig varchar(5), Dest varchar(5), Miles int, Boarded int, Capacity double)";
+      String partitionedColumns =
+          "(Flight int, Dates varchar(255), Depart varchar(10), Orig varchar(5), Dest varchar(5), Miles int, Boarded int)";
+      String localFilepath = f + "/samdat1.csv";
+      String HdfsURI = "hdfs://" + hdfsConnection;
+      String filePath = "/tmp/htest/00000_";
+      String fileDestination = HdfsURI + filePath;
+      assertFalse(con.getMetaData().supportsRefCursors());
+      assertTrue(con.getMetaData().allTablesAreSelectable());
+      System.out.println(con.getMetaData().getDatabaseProductName());
+      System.out.println(
+          "Hive Version: " + con.getMetaData().getDatabaseMajorVersion() + "."
+              + con.getMetaData().getDatabaseMinorVersion());
+      getTables(con, newTableName);
+      dropTable(stmt, newTableName);
+      dropTable(stmt, newTableName + "NT");
+      dropTable(stmt, newTableName + "T");
+      dropTable(stmt, newTableName + "P");
+      dropTable(stmt, newTableName + "V");
+      showTables(stmt, "show tables like 'b*'");
+      createTable(stmt, newTableName, columnNames, ",", "");
+      try {
+        createTable(stmt, newTableName + "NT", columnNames, ",",
+            "TBLPROPERTIES(\"transactional\"=\"true\")");
+        fail("shouldn't get here");
+      } catch (SQLException e) {
+
+      }
+      createTable(stmt, newTableName + "T", columnNames, ",",
+          "STORED AS ORC TBLPROPERTIES(\"transactional\"=\"true\")");
+      createPartitionedTable(stmt, newTableName + "P", partitionedColumns,
+          "(Capacity double)", ",", "STORED AS ORC");
+      createTable(stmt, newTableName + "V", "(Dest varchar(5))", ",",
+          "STORED AS ORC");
+      showTables(stmt, "show tables like 'b*'");
+      loadFile(localFilepath, HdfsURI, fileDestination + ".txt");
+      loadData(stmt, filePath + ".txt", newTableName);
+      describeTable(stmt, newTableName);
+      updateTable(stmt, "Insert into table btestt SELECT * from btest");
+      updateTable(stmt, "Insert into table btestv SELECT Dest from btest");
+      updateTable(stmt,
+          "Insert into table btestp PARTITION (Capacity) SELECT * from btest");
+      deleteFile(stmt, filePath + ".txt", HdfsURI);
+      deleteFile(stmt, filePath + "0.orc", HdfsURI);
+      assertEquals("302", printResults(stmt, "Select * from btest"));
+      assertEquals("114", printResults(stmt,
+          "Select * from btest where Dest = 'LAX' order by boarded desc"));
+      assertEquals("622", printResults(stmt,
+          "Select * from btest where boarded between 160 and 180 order by boarded asc"));
+      assertEquals("202", printResults(stmt,
+          "Select * from btest where Dest='LAX' or Dest='ORD'"));
+      assertEquals("114", printResults(stmt,
+          "Select * from btest where Dest= 'LAX' and boarded >= 180"));
+      assertEquals("114", printResults(stmt,
+          "Select * from btest where Dest ='LAX' and boarded = 197"));
+      assertEquals("114", preparedStatement(con,
+          "Select * from btest where Dest ='LAX' and boarded = 197"));
+      assertEquals(15, setFetchSizeStatement(stmt));
+      assertEquals(15, setFetchSizePreparedStatement(con));
+      // assertEquals(callableStatement(con, -20), -1);
+      assertEquals("95487", printResults(stmt, "Select SUM(Miles) from btest"));
+      assertEquals("302", printResults(stmt, "Select * from btest"));
+      assertEquals(0, updateTable(stmt,
+          "Update btestt set Orig= 'test' where Dest= 'LAX'"));
+      try {
+        loadData(stmt, filePath + ".txt", newTableName + "T");
+        fail("shouldn't get here");
+      } catch (HiveSQLException e) {
+        System.out.println("File does not exist in specified location");
+      }
+      assertEquals("132",
+          printResults(stmt, "Select * from btestt order by Dest"));
+      assertEquals("capacity=250.0",
+          printResults(stmt, "show partitions btestp"));
+      assertEquals("LAX", printResults(stmt,
+          "select MIN(Dest), boarded from btest where Dest ='LAX' group by Dest, boarded"));
+      executeStatement(stmt, "Drop view testview");
+      executeStatement(stmt,
+          "Create view testview AS select SUM(btest.boarded) BOARDED from btest, btestv where btest.Dest=btestv.Dest");
+      assertEquals("45072", printResults(stmt, "Select * from testview"));
+      executeStatement(stmt, "Drop view testview");
+      printResults(stmt, "Describe formatted btest");
+      dropTable(stmt, newTableName);
+      dropTable(stmt, newTableName + "NT");
+      dropTable(stmt, newTableName + "T");
+      dropTable(stmt, newTableName + "P");
+      dropTable(stmt, newTableName + "V");
+      setNegativeFetchSize(stmt);
+    }
+  }
+
+  @Test
+  public void testFunctionsandMetaData() throws Exception {
+
+    String queryValues = "insert into table btestm values"
+        + " ('Alfred', 'M', 69.0, 122.5, 'AJH', 1),"
+        + " ('Alfred', 'M', 71.0, 130.5, 'AJH', 2),"
+        + " ('Alicia', 'F', 56.5, 84.0, 'BJH', 1),"
+        + " ('Alicia', 'F', 60.5, 86.9, 'BJH', 2),"
+        + " ('Benicia', 'F', 65.3, 98.0, 'BJH', 1),"
+        + " ('Benicia', 'F', 69.3, 99.1, 'BJH', 2),"
+        + " ('Bennett', 'F', 63.2, 96.2, 'AJH', 1),"
+        + " ('Bennett', 'F', 69.2, 98.2, 'AJH', 2),"
+        + " ('Carol', 'F', 62.8, 102.5, 'BJH', 1),"
+        + " ('Carol', 'F', 65.3, 105.4, 'BJH', 2),"
+        + " ('Carlos', 'M', 63.7, 102.9, 'AJH', 1),"
+        + " ('Carlos', 'M', 70.3, 106.9, 'AJH', 2),"
+        + " ('Henry', 'M', 63.5, 102.5, 'AJH', 1),"
+        + " ('Henry', 'M', 68.9, 108.6, 'AJH', 2),"
+        + " ('Jaime', 'M', 57.3, 86.0, 'BJH', 1),"
+        + " ('Jaime', 'M', 62.9, 90.0, 'BJH', 2),"
+        + " ('Janet', 'F', 59.8, 84.5, 'AJH', 1),"
+        + " ('Janet', 'F', 62.5, 86.5, 'AJH', 2),"
+        + " ('Jean', 'M', 68.2, 113.4, 'AJH', 1),"
+        + " ('Jean', 'M', 70.3, 116.0, 'AJH', 2),"
+        + " ('Joyce', 'M', 51.3, 50.5, 'BJH', 1),"
+        + " ('Joyce', 'M', 55.5, 53.5, 'BJH', 2),"
+        + " ('Luc', 'M', 66.3, 77.0, 'AJH', 1),"
+        + " ('Luc', 'M', 69.3, 82.9, 'AJH', 2),"
+        + " ('Marie', 'F', 66.5, 112.0, 'BJH', 1),"
+        + " ('Marie', 'F', 69.5, 114.9, 'BJH', 2),"
+        + " ('Medford', 'M', 64.9, 114.0, 'AJH', 1),"
+        + " ('Medford', 'M', NULL, NULL, NULL, NULL),"
+        + " ('Philip', 'M', 69.0, 115.0, 'AJH', 1),"
+        + " ('Philip', 'M', 70.0, 118.0, 'AJH', 2),"
+        + " ('Robert', 'M', 64.8, 128.0, 'BJH', 1),"
+        + " ('Robert', 'M', 68.3, NULL, 'BJH', 2),"
+        + " ('Thomas', 'M', 57.5, 85.0, 'AJH', 1),"
+        + " ('Thomas', 'M', 59.1, 92.3, 'AJH', 2),"
+        + " ('Wakana', 'F', 61.3, 99.0, 'AJH', 1),"
+        + " ('Wakana', 'F', 63.8, 102.9, 'AJH', 2),"
+        + " ('William', 'M', 66.5, 112.0, 'BJH', 1),"
+        + " ('William', 'M', 68.3, 118.2, 'BJH', 2)";
+
+    // failing query from SAS Institute, code generated by PROC MEANS
+    String failingQuery =
+        " select COUNT(*) as ZSQL1, MIN(TXT_1.`school`) as ZSQL2, MIN(TXT_1.`time`) as"
+            + " ZSQL3, COUNT(*) as ZSQL4, COUNT(TXT_1.`height`) as ZSQL5, MIN(TXT_1.`height`)"
+            + " as ZSQL6, MAX(TXT_1.`height`) as ZSQL7, SUM(TXT_1.`height`) as ZSQL8,"
+            + " COALESCE(VAR_SAMP(TXT_1.`height`)*(COUNT(TXT_1.`height`)-1),0) as ZSQL9,"
+            + " COUNT(TXT_1.`weight`) as ZSQL10, MIN(TXT_1.`weight`) as ZSQL11,"
+            + " MAX(TXT_1.`weight`) as ZSQL12, SUM(TXT_1.`weight`) as ZSQL13,"
+            + " COALESCE(VAR_SAMP(TXT_1.`weight`)*(COUNT(TXT_1.`weight`)-1),0) as ZSQL14 from"
+            + " `btestm` TXT_1 where TXT_1.`school` = 'AJH' group by"
+            + " TXT_1.`school`, TXT_1.`time`";
+
+    try (Statement stmt = con.createStatement()) {
+      dropTable(stmt, newTableName + "M");
+      stmt.executeUpdate(
+          "create table btestm (name varchar(8), sex varchar(8), height double, weight double, school varchar(8), `time` double)");
+      stmt.executeUpdate(queryValues);
+
+      try (ResultSet res = stmt.executeQuery(failingQuery)) {
+        assertTrue(res.next());
+        assertEquals(null, res.getString(2));
+        ResultSetMetaData rsmd = res.getMetaData();
+        assertEquals(19, rsmd.getPrecision(1));
+        assertEquals(-5, rsmd.getColumnType(1));
+        assertEquals(14, rsmd.getColumnCount());
+      }
+      dropTable(stmt, newTableName + "M");
+    }
+  }
 }
