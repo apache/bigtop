987450edd7094b972537cd162bf82666c5fa6602
Epikhin Mikhail <schizophrenia@yandex-team.ru>
2018-07-16 12:21:59 +0300

    Add support ubuntu 18.04

--- bigtop_toolchain/bin/puppetize.sh
+++ bigtop_toolchain/bin/puppetize.sh
@@ -26,7 +26,7 @@ case ${ID}-${VERSION_ID} in
       dnf -y check-update
       dnf -y install hostname findutils curl sudo unzip wget puppet puppetlabs-stdlib
         ;;
-    ubuntu-16.04)
+    ubuntu-1[6,8].04)
         apt-get update
         apt-get -y install wget curl sudo unzip puppet software-properties-common puppet-module-puppetlabs-apt puppet-module-puppetlabs-stdlib
         ;;
