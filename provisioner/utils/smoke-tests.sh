041307fe0b19f0547332bfd7c3fee269a3d1af50
Evans Ye <evansye@apache.org>
2019-03-28 15:41:21 +0800

    BIGTOP-3199. Can not run smoke test inside docker-provisioner

--- provisioner/utils/smoke-tests.sh
+++ provisioner/utils/smoke-tests.sh
@@ -80,8 +80,9 @@ ALL_SMOKE_TASKS=""
 for s in `echo $SMOKE_TESTS | sed -e 's#,# #g'`; do
   ALL_SMOKE_TASKS="$ALL_SMOKE_TASKS bigtop-tests:smoke-tests:$s:test"
 done
+rm -rf /bigtop-home/.gradle
 cd /bigtop-home && ./gradlew clean $ALL_SMOKE_TASKS -Psmoke.tests --info
 # BIGTOP-2244 workaround: clean the top level buildSrc/build with the same
 # permissions as used for smoke-tests execution
 rm -rf buildSrc/build/test-results/binary
-
+rm -rf /bigtop-home/.gradle
