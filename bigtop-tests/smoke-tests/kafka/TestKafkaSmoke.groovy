00f45883ae590463d5d2995c0a723021f0967109
Yuqi Gu <yuqi.gu@arm.com>
2019-07-18 07:18:34 +0000

    Remove 'ZK Server start' and 'Kafka server start'
    Change-Id: I50a7ccae5249a816772e0dce7ff0c294582d1001

--- bigtop-tests/smoke-tests/kafka/TestKafkaSmoke.groovy
+++ bigtop-tests/smoke-tests/kafka/TestKafkaSmoke.groovy
@@ -31,18 +31,11 @@ import org.junit.runner.RunWith
 class TestKafkaSmoke {
   static Shell sh = new Shell("/bin/bash -s");
 
-  static final String ZOOKEEPER_HOME = "/usr/lib/zookeeper"
-  static final String ZK_SERVER = ZOOKEEPER_HOME + "/bin/zkServer.sh"
-
   static final String KAFKA_HOME = "/usr/lib/kafka"
-  static final String KAFKA_CONFIG = KAFKA_HOME + "/config/server.properties"
   static final String KAFKA_TOPICS = KAFKA_HOME + "/bin/kafka-topics.sh"
-  static final String KAFKA_SERVER_START = KAFKA_HOME + "/bin/kafka-server-start.sh"
 
   @Test
   public void testCreateTopics() {
-    sh.exec(ZK_SERVER + " start");
-    sh.exec(KAFKA_SERVER_START + KAFKA_CONFIG + " &");
     sh.exec(KAFKA_TOPICS + " --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic test");
     sh.exec(KAFKA_TOPICS + " --list --zookeeper localhost:2181");
     assertTrue(" Create Kafka topics failed. " + sh.getOut() + " " + sh.getErr(), sh.getRet() == 0);
