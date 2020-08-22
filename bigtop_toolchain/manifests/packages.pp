b2168365495a6cc01fc1b861fca85f325c6221e9
Kengo Seki <sekikn@apache.org>
2020-08-22 23:19:48 +0900

    Add net-tools to the package list for CentOS/Fedora

--- bigtop_toolchain/manifests/packages.pp
+++ bigtop_toolchain/manifests/packages.pp
@@ -63,6 +63,7 @@ class bigtop_toolchain::packages {
         "bison",
         "libffi-devel",
         "krb5-devel",
+        "net-tools",
       ]
       if ($operatingsystem == 'Fedora' or $operatingsystemmajrelease !~ /^[0-7]$/) {
         $pkgs = concat($_pkgs, ["python2-devel", "libtirpc-devel"])
