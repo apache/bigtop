b9821120ef304f72ff8512325827752916a673e2
Masatake Iwasaki <iwasakims@apache.org>
2020-09-18 11:35:36 +0000

    smoke_test_components should be fixed too.

--- provisioner/docker/docker-hadoop.sh
+++ provisioner/docker/docker-hadoop.sh
@@ -191,7 +191,7 @@ provision() {
 smoke-tests() {
     hadoop_head_node=${NODES:0:12}
     if [ -z ${smoke_test_components+x} ]; then
-        smoke_test_components="`echo "$(get-yaml-config smoke_test_components)" | sed 's/ /,/g'`"
+        smoke_test_components="`echo $(get-yaml-config smoke_test_components) | sed 's/ /,/g'`"
     fi
     docker exec $hadoop_head_node bash -c "bash -x /bigtop-home/provisioner/utils/smoke-tests.sh $smoke_test_components"
 }
