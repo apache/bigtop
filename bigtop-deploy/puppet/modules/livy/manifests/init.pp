af15381e24d9095270094eb43e1dfb164c2f91c0
Kengo Seki <sekikn@apache.org>
2020-12-04 21:54:29 +0900

    BIGTOP-3462. Deploying Livy fails if the SPARK_HOME directory doesn't exist.

--- bigtop-deploy/puppet/modules/livy/manifests/init.pp
+++ bigtop-deploy/puppet/modules/livy/manifests/init.pp
@@ -43,10 +43,13 @@ class livy {
 
     service { 'livy-server':
       ensure     => running,
-      require    => Package['livy'],
+      require    => [
+        Package['spark-core'],
+        Package['livy'],
+      ],
       hasrestart => true,
       hasstatus  => true,
-      subscribe => [
+      subscribe  => [
         File['/etc/livy/conf/livy-env.sh'],
         File['/etc/livy/conf/livy.conf']
       ]
