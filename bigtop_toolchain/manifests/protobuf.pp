87a48df9aca268d9cb597938b9d9bca59d84076b
Masatake Iwasaki <iwasakims@apache.org>
2021-06-23 10:45:55 +0000

    added ppc64le patch for protoc.

--- bigtop_toolchain/manifests/protobuf.pp
+++ bigtop_toolchain/manifests/protobuf.pp
@@ -48,11 +48,16 @@ class bigtop_toolchain::protobuf {
       creates => "/usr/src/protobuf-3.5.1.1",
     }
 
+    file { "/usr/src/protobuf-3.5.1.1/protoc-3.5.1-add-support-for-ppc64le.patch":
+      source => "puppet:///modules/bigtop_toolchain/protoc-3.5.1-add-support-for-ppc64le.patch",
+      require => Exec["download protobuf 3.5.1.1"],
+    }
+
     exec { "install protobuf 3.5.1.1":
       cwd => "/usr/src/protobuf-3.5.1.1",
-      command => "/usr/src/protobuf-3.5.1.1/autogen.sh && /usr/src/protobuf-3.5.1.1/configure --prefix=/usr/local/protobuf-3.5.1.1 --disable-shared --with-pic && /usr/bin/make install",
+      command => "/usr/bin/patch -p1 < protoc-3.5.1-add-support-for-ppc64le.patch && /usr/src/protobuf-3.5.1.1/autogen.sh && /usr/src/protobuf-3.5.1.1/configure --prefix=/usr/local/protobuf-3.5.1.1 --disable-shared --with-pic && /usr/bin/make install",
       creates => "/usr/local/protobuf-3.5.1.1",
-      require => Exec["download protobuf 3.5.1.1"],
+      require => File["/usr/src/protobuf-3.5.1.1/protoc-3.5.1-add-support-for-ppc64le.patch"],
       timeout => 3000
     }
   }
