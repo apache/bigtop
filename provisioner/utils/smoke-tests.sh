ee4c38b84e11e73e80a1b2f28a764ac66874fd2e
Kengo Seki <sekikn@apache.org>
2021-08-26 00:19:22 +0900

    BIGTOP-3577. HBase smoke test partially fails on ppc64le.

--- provisioner/utils/smoke-tests.sh
+++ provisioner/utils/smoke-tests.sh
@@ -81,7 +81,7 @@ for s in `echo $SMOKE_TESTS | sed -e 's#,# #g'`; do
   ALL_SMOKE_TASKS="$ALL_SMOKE_TASKS bigtop-tests:smoke-tests:$s:test"
 done
 rm -rf /bigtop-home/.gradle
-cd /bigtop-home && ./gradlew clean $ALL_SMOKE_TASKS -Psmoke.tests --info
+cd /bigtop-home && ./gradlew clean $ALL_SMOKE_TASKS -Psmoke.tests -Duser.dir=/bigtop-home --info
 # BIGTOP-2244 workaround: clean the top level buildSrc/build with the same
 # permissions as used for smoke-tests execution
 rm -rf buildSrc/build/test-results/binary
