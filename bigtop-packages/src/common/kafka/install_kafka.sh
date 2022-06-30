1cbd5e3c0239d14345235ec689b0d03fe1b484db
yoda-mon <yodal@oss.nttdata.com>
2022-06-30 10:52:44 +0000

    add symlink on zookeeper side and link it again from kafka side

--- bigtop-packages/src/common/kafka/install_kafka.sh
+++ bigtop-packages/src/common/kafka/install_kafka.sh
@@ -89,10 +89,6 @@ for var in PREFIX BUILD_DIR SOURCE_DIR; do
   fi
 done
 
-if [ -f "$SOURCE_DIR/bigtop.bom" ]; then
-  . $SOURCE_DIR/bigtop.bom
-fi
-
 MAN_DIR=${MAN_DIR:-/usr/share/man/man1}
 DOC_DIR=${DOC_DIR:-/usr/share/doc/kafka}
 LIB_DIR=${LIB_DIR:-/usr/lib/kafka}
@@ -169,7 +165,7 @@ rm -f ${PREFIX}/${LIB_DIR}/libs/zookeeper-*.jar
 ln -sf /usr/lib/zookeeper/zookeeper.jar ${PREFIX}/${LIB_DIR}/libs/
 
 # BIGTOP-3712
-ln -s /usr/lib/zookeeper/zookeeper-jute-${ZOOKEEPER_VERSION}.jar ${PREFIX}/${LIB_DIR}/libs/
+ln -s /usr/lib/zookeeper/zookeeper-jute.jar ${PREFIX}/${LIB_DIR}/libs/
 
 # Copy in the defaults file
 install -d -m 0755 ${PREFIX}/etc/default
