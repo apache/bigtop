3531120f73c249ccd5c1c8937683511be067a3f8
Leona Yoda <yodal@oss.nttdata.com>
2022-07-04 18:04:47 +0900

    BIGTOP-3711: Add read/write smoke tests for Kafka (#922)
    * Add read/write test for a kafka topic
    
    * reorder import statements
    
    * add a timeout option to consumer

--- bigtop-tests/smoke-tests/kafka/TestKafkaSmoke.groovy
+++ bigtop-tests/smoke-tests/kafka/TestKafkaSmoke.groovy
@@ -18,21 +18,28 @@
 
 package org.apache.bigtop.itest.kafka
 
-import org.junit.BeforeClass
-import org.junit.AfterClass
-import org.apache.bigtop.itest.shell.Shell
-import static org.junit.Assert.assertNotNull
-import static org.junit.Assert.assertTrue
-import org.junit.Test
 import org.apache.bigtop.itest.JarContent
 import org.apache.bigtop.itest.TestUtils
+import org.apache.bigtop.itest.shell.Shell
+import org.junit.AfterClass
+import org.junit.BeforeClass
+import org.junit.FixMethodOrder
+import org.junit.Test
 import org.junit.runner.RunWith
+import org.junit.runners.MethodSorters
 
+import static org.junit.Assert.assertNotNull
+import static org.junit.Assert.assertTrue
+
+@FixMethodOrder (MethodSorters.NAME_ASCENDING)
 class TestKafkaSmoke {
   static Shell sh = new Shell("/bin/bash -s");
 
   static final String KAFKA_HOME = "/usr/lib/kafka"
   static final String KAFKA_TOPICS = KAFKA_HOME + "/bin/kafka-topics.sh "
+  static final String KAFKA_PRODUCER = KAFKA_HOME + "/bin/kafka-console-producer.sh"
+  static final String KAFKA_CONSUMER = KAFKA_HOME + "/bin/kafka-console-consumer.sh"
+  static final String TEST_MESSAGE = "Hello Bigtop"
 
   @AfterClass
   public static void deleteKafkaTopics() {
@@ -44,9 +51,28 @@ class TestKafkaSmoke {
   }
 
   @Test
-  public void testCreateTopics() {
+  public void test0CreateTopics() {
     sh.exec(KAFKA_TOPICS + " --create --zookeeper localhost:2181 --replication-factor 1 --partitions 1 --topic test");
     sh.exec(KAFKA_TOPICS + " --list --zookeeper localhost:2181");
     assertTrue(" Create Kafka topics failed. " + sh.getOut() + " " + sh.getErr(), sh.getRet() == 0);
+  }
+
+  @Test
+  public void test1WriteKafkaTopics() {
+    sh.exec("echo '" + TEST_MESSAGE + "' | " + KAFKA_PRODUCER + " --broker-list localhost:9092 --topic test");
+    assertTrue(
+      " Write Kafka topics failed. " + sh.getOut() + " " + sh.getErr(),
+      sh.getRet() == 0
+    );
+  }
+
+  @Test
+  public void test2ReadKafkaTopics() {
+    sh.exec(KAFKA_CONSUMER + " --bootstrap-server localhost:9092 --topic test \
+            --partition 0 --offset 0 --max-messages 1 --timeout-ms 10000");
+    assertTrue(
+      " Read Kafka topics failed. " + sh.getOut() + " " + sh.getErr(),
+      sh.getOut() == [TEST_MESSAGE]
+    );
   }
 }
