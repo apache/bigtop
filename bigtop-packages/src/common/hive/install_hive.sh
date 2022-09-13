c8c4e88f5b2d1ac6b4b00bb59ceff4dfd7131b7c
Zhiguo Wu <wuzhiguo@apache.org>
2022-09-13 13:57:37 +0800

    BIGTOP-3799: Support parent directory configuration for Hive rpm build script (#998)
    * BIGTOP-3799: Support parent directory configuration for Hive rpm build script
    
    * remove docdir prefix on deb build script

--- bigtop-packages/src/common/hive/install_hive.sh
+++ bigtop-packages/src/common/hive/install_hive.sh
@@ -24,12 +24,15 @@ usage: $0 <options>
 
   Optional options:
      --doc-dir=DIR               path to install docs into [/usr/share/doc/hive]
-     --hive-dir=DIR               path to install hive home [/usr/lib/hive]
-     --installed-hive-dir=DIR     path where hive-dir will end up on target system
      --bin-dir=DIR               path to install bins [/usr/bin]
+     --man-dir=DIR               path to install mans [/usr/share/man]
+     --etc-default=DIR           path to bigtop default dir [/etc/default]
+     --hive-dir=DIR              path to install hive home [/usr/lib/hive]
+     --var-hive-dir=DIR          path to install hive contents [/var/lib/hive]
+     --hcatalog-dir=DIR          path to install hcatalog home [/usr/lib/hcatalog]
+     --var-hcatalog-dir=DIR      path to install hcatalog contents [/var/lib/hcatalog]
+     --etc-hive=DIR              path to install hive conf [/etc/hive]
      --examples-dir=DIR          path to install examples [doc-dir/examples]
-     --hcatalog-dir=DIR          path to install hcatalog [/usr/lib/hcatalog]
-     --installed-hcatalog-dir=DIR path where hcatalog-dir will end up on target system
      ... [ see source for more similar options ]
   "
   exit 1
@@ -40,13 +43,16 @@ OPTS=$(getopt \
   -o '' \
   -l 'prefix:' \
   -l 'doc-dir:' \
-  -l 'hive-dir:' \
-  -l 'installed-hive-dir:' \
   -l 'bin-dir:' \
+  -l 'man-dir:' \
+  -l 'etc-default:' \
+  -l 'hive-dir:' \
+  -l 'var-hive-dir:' \
+  -l 'hcatalog-dir:' \
+  -l 'var-hcatalog-dir:' \
+  -l 'etc-hive:' \
   -l 'examples-dir:' \
   -l 'python-dir:' \
-  -l 'hcatalog-dir:' \
-  -l 'installed-hcatalog-dir:' \
   -l 'build-dir:' \
   -l 'hive-version:' -- "$@")
 
@@ -66,26 +72,35 @@ while true ; do
         --doc-dir)
         DOC_DIR=$2 ; shift 2
         ;;
+        --bin-dir)
+        BIN_DIR=$2 ; shift 2
+        ;;
+        --man-dir)
+        MAN_DIR=$2 ; shift 2
+        ;;
+        --etc-default)
+        ETC_DEFAULT=$2 ; shift 2
+        ;;
         --hive-dir)
         HIVE_DIR=$2 ; shift 2
         ;;
-        --installed-hive-dir)
-        INSTALLED_HIVE_DIR=$2 ; shift 2
+        --var-hive-dir)
+        VAR_HIVE_DIR=$2 ; shift 2
         ;;
-        --bin-dir)
-        BIN_DIR=$2 ; shift 2
+        --hcatalog-dir)
+        HCATALOG_DIR=$2 ; shift 2
+        ;;
+        --var-hcatalog-dir)
+        VAR_HCATALOG_DIR=$2 ; shift 2
+        ;;
+        --etc-hive)
+        ETC_HIVE=$2 ; shift 2
         ;;
         --examples-dir)
         EXAMPLES_DIR=$2 ; shift 2
         ;;
         --python-dir)
         PYTHON_DIR=$2 ; shift 2
-        ;;
-        --hcatalog-dir)
-        HCATALOG_DIR=$2 ; shift 2
-        ;;
-        --installed-hcatalog-dir)
-        INSTALLED_HCATALOG_DIR=$2 ; shift 2
         ;;
         --hive-version)
         HIVE_VERSION=$2 ; shift 2
@@ -108,37 +123,40 @@ for var in PREFIX BUILD_DIR ; do
   fi
 done
 
-MAN_DIR=$PREFIX/usr/share/man/man1
-DOC_DIR=${DOC_DIR:-$PREFIX/usr/share/doc/hive}
-HIVE_DIR=${HIVE_DIR:-$PREFIX/usr/lib/hive}
-INSTALLED_HIVE_DIR=${INSTALLED_HIVE_DIR:-/usr/lib/hive}
-EXAMPLES_DIR=${EXAMPLES_DIR:-$DOC_DIR/examples}
-BIN_DIR=${BIN_DIR:-$PREFIX/usr/bin}
-PYTHON_DIR=${PYTHON_DIR:-$HIVE_DIR/lib/py}
-HCATALOG_DIR=${HCATALOG_DIR:-$PREFIX/usr/lib/hive-hcatalog}
+MAN_DIR=${MAN_DIR:-/usr/share/man}/man1
+DOC_DIR=${DOC_DIR:-/usr/share/doc/hive}
+BIN_DIR=${BIN_DIR:-/usr/bin}
+ETC_DEFAULT=${ETC_DEFAULT:-/etc/default}
+HIVE_DIR=${HIVE_DIR:-/usr/lib/hive}
+VAR_HIVE_DIR=${VAR_HIVE_DIR:-/var/lib/hive}
+HCATALOG_DIR=${HCATALOG_DIR:-/usr/lib/hive-hcatalog}
+VAR_HCATALOG_DIR=${VAR_HCATALOG_DIR:-/var/lib/hive-hcatalog}
 HCATALOG_SHARE_DIR=${HCATALOG_DIR}/share/hcatalog
-INSTALLED_HCATALOG_DIR=${INSTALLED_HCATALOG_DIR:-/usr/lib/hive-hcatalog}
-CONF_DIR=/etc/hive
-CONF_DIST_DIR=/etc/hive/conf.dist
+EXAMPLES_DIR=${EXAMPLES_DIR:-$DOC_DIR/examples}
+PYTHON_DIR=${PYTHON_DIR:-$HIVE_DIR/lib/py}
+
+ETC_HIVE=${ETC_HIVE:-/etc/hive}
+# No prefix
+NP_ETC_HIVE=/etc/hive
 
 # First we'll move everything into lib
-install -d -m 0755 ${HIVE_DIR}
-(cd ${BUILD_DIR} && tar -cf - .)|(cd ${HIVE_DIR} && tar -xf -)
-rm -f ${HIVE_DIR}/lib/hive-shims-0.2*.jar
-for jar in `ls ${HIVE_DIR}/lib/hive-*.jar | grep -v 'standalone.jar'`; do
+install -d -m 0755 $PREFIX/$HIVE_DIR
+(cd ${BUILD_DIR} && tar -cf - .)|(cd $PREFIX/$HIVE_DIR && tar -xf -)
+rm -f $PREFIX/$HIVE_DIR/lib/hive-shims-0.2*.jar
+for jar in `ls $PREFIX/$HIVE_DIR/lib/hive-*.jar | grep -v 'standalone.jar'`; do
     base=`basename $jar`
-    (cd ${HIVE_DIR}/lib && ln -s $base ${base/-[0-9].*/.jar})
+    (cd $PREFIX/$HIVE_DIR/lib && ln -s $base ${base/-[0-9].*/.jar})
 done
 
 for thing in conf README.txt examples lib/py;
 do
-  rm -rf ${HIVE_DIR}/$thing
+  rm -rf $PREFIX/$HIVE_DIR/$thing
 done
 
-install -d -m 0755 ${BIN_DIR}
+install -d -m 0755 $PREFIX/$BIN_DIR
 for file in hive beeline hiveserver2
 do
-  wrapper=$BIN_DIR/$file
+  wrapper=$PREFIX/$BIN_DIR/$file
   cat >>$wrapper <<EOF
 #!/bin/bash
 
@@ -147,78 +165,80 @@ if [ -e /usr/lib/bigtop-utils/bigtop-detect-javahome ]; then
   . /usr/lib/bigtop-utils/bigtop-detect-javahome
 fi
 
-BIGTOP_DEFAULTS_DIR=\${BIGTOP_DEFAULTS_DIR-/etc/default}
+BIGTOP_DEFAULTS_DIR=\${BIGTOP_DEFAULTS_DIR-$ETC_DEFAULT}
 [ -n "\${BIGTOP_DEFAULTS_DIR}" -a -r \${BIGTOP_DEFAULTS_DIR}/hbase ] && . \${BIGTOP_DEFAULTS_DIR}/hbase
 
-export HIVE_HOME=$INSTALLED_HIVE_DIR
-exec $INSTALLED_HIVE_DIR/bin/$file "\$@"
+export HIVE_HOME=$HIVE_DIR
+exec $HIVE_DIR/bin/$file "\$@"
 EOF
   chmod 755 $wrapper
 done
 
 # Config
-install -d -m 0755 ${PREFIX}${CONF_DIST_DIR}
-(cd ${BUILD_DIR}/conf && tar -cf - .)|(cd ${PREFIX}${CONF_DIST_DIR} && tar -xf -)
+install -d -m 0755 $PREFIX/$NP_ETC_HIVE
+install -d -m 0755 $PREFIX/$ETC_HIVE/conf.dist
+(cd ${BUILD_DIR}/conf && tar -cf - .)|(cd $PREFIX/$ETC_HIVE/conf.dist && tar -xf -)
 for template in hive-exec-log4j2.properties hive-log4j2.properties
 do
-  mv ${PREFIX}${CONF_DIST_DIR}/${template}.template ${PREFIX}${CONF_DIST_DIR}/${template}
+  mv $PREFIX/$ETC_HIVE/conf.dist/${template}.template $PREFIX/$ETC_HIVE/conf.dist/${template}
 done
-cp hive-site.xml ${PREFIX}${CONF_DIST_DIR}
-sed -i -e "s|@VERSION@|${HIVE_VERSION}|" ${PREFIX}${CONF_DIST_DIR}/hive-site.xml
+cp hive-site.xml $PREFIX/$ETC_HIVE/conf.dist
+sed -i -e "s|@VERSION@|${HIVE_VERSION}|" $PREFIX/$ETC_HIVE/conf.dist/hive-site.xml
 
-ln -s ${CONF_DIR}/conf $HIVE_DIR/conf
+ln -s $NP_ETC_HIVE/conf $PREFIX/$HIVE_DIR/conf
 
-install -d -m 0755 $MAN_DIR
-gzip -c hive.1 > $MAN_DIR/hive.1.gz
+install -d -m 0755 $PREFIX/$MAN_DIR
+gzip -c hive.1 > $PREFIX/$MAN_DIR/hive.1.gz
 
 # Docs
-install -d -m 0755 ${DOC_DIR}
-cp ${BUILD_DIR}/README.txt ${DOC_DIR}
-mv ${HIVE_DIR}/NOTICE ${DOC_DIR}
-mv ${HIVE_DIR}/LICENSE ${DOC_DIR}
-mv ${HIVE_DIR}/RELEASE_NOTES.txt ${DOC_DIR}
+install -d -m 0755 $PREFIX/$DOC_DIR
+mv $PREFIX/$HIVE_DIR/NOTICE $PREFIX/$DOC_DIR
+mv $PREFIX/$HIVE_DIR/LICENSE $PREFIX/$DOC_DIR
+mv $PREFIX/$HIVE_DIR/RELEASE_NOTES.txt $PREFIX/$DOC_DIR
 
 
 # Examples
-install -d -m 0755 ${EXAMPLES_DIR}
-cp -a ${BUILD_DIR}/examples/* ${EXAMPLES_DIR}
+install -d -m 0755 $PREFIX/$EXAMPLES_DIR
+cp -a ${BUILD_DIR}/examples/* $PREFIX/$EXAMPLES_DIR
 
 # Python libs
-install -d -m 0755 ${PYTHON_DIR}
-(cd $BUILD_DIR/lib/py && tar cf - .) | (cd ${PYTHON_DIR} && tar xf -)
-chmod 755 ${PYTHON_DIR}/hive_metastore/*-remote
+install -d -m 0755 $PREFIX/$PYTHON_DIR
+(cd $BUILD_DIR/lib/py && tar cf - .) | (cd $PREFIX/$PYTHON_DIR && tar xf -)
+chmod 755 $PREFIX/$PYTHON_DIR/hive_metastore/*-remote
 
 # Dir for Metastore DB
-install -d -m 1777 $PREFIX/var/lib/hive/metastore/
+install -d -m 1777 $PREFIX/$VAR_HIVE_DIR/metastore/
 
 # We need to remove the .war files. No longer supported.
-rm -f ${HIVE_DIR}/lib/hive-hwi*.war
+rm -f $PREFIX/$HIVE_DIR/lib/hive-hwi*.war
 
 # Remove some source which gets installed
-rm -rf ${HIVE_DIR}/lib/php/ext
+rm -rf $PREFIX/$HIVE_DIR/lib/php/ext
 
-install -d -m 0755 ${HCATALOG_DIR}
-mv ${HIVE_DIR}/hcatalog/* ${HCATALOG_DIR}
-rm -rf ${HIVE_DIR}/hcatalog
+install -d -m 0755 $PREFIX/$HCATALOG_DIR
+mv $PREFIX/$HIVE_DIR/hcatalog/* $PREFIX/$HCATALOG_DIR
+rm -rf $PREFIX/$HIVE_DIR/hcatalog
 
 # Workaround for HIVE-5534
-find ${HCATALOG_DIR} -name *.sh | xargs chmod 755
-chmod 755 ${HCATALOG_DIR}/bin/hcat
+find $PREFIX/$HCATALOG_DIR -name *.sh | xargs chmod 755
+chmod 755 $PREFIX/$HCATALOG_DIR/bin/hcat
 
-install -d -m 0755 ${PREFIX}/etc/default
-for conf in `cd ${HCATALOG_DIR}/etc ; ls -d *` ; do
-  install -d -m 0755 ${PREFIX}/etc/hive-$conf
-  mv ${HCATALOG_DIR}/etc/$conf ${PREFIX}/etc/hive-$conf/conf.dist
-  ln -s /etc/hive-$conf/conf ${HCATALOG_DIR}/etc/$conf
-  touch ${PREFIX}/etc/default/hive-$conf-server
+install -d -m 0755 $PREFIX/$ETC_DEFAULT
+for conf in `cd $PREFIX/$HCATALOG_DIR/etc ; ls -d *` ; do
+  install -d -m 0755 ${PREFIX}/$NP_ETC_HIVE-$conf
+  install -d -m 0755 ${PREFIX}/$ETC_HIVE-$conf
+  mv $PREFIX/$HCATALOG_DIR/etc/$conf ${PREFIX}/$ETC_HIVE-$conf/conf.dist
+  ln -s $NP_ETC_HIVE-$conf/conf $PREFIX/$HCATALOG_DIR/etc/$conf
+  touch $PREFIX/$ETC_DEFAULT/hive-$conf-server
 done
 
-wrapper=$BIN_DIR/hcat
+wrapper=$PREFIX/$BIN_DIR/hcat
 cat >>$wrapper <<EOF
 #!/bin/sh
 
-BIGTOP_DEFAULTS_DIR=${BIGTOP_DEFAULTS_DIR-/etc/default}
-[ -n "${BIGTOP_DEFAULTS_DIR}" -a -r ${BIGTOP_DEFAULTS_DIR}/hadoop ] && . ${BIGTOP_DEFAULTS_DIR}/hadoop
+
+BIGTOP_DEFAULTS_DIR=\${BIGTOP_DEFAULTS_DIR-$ETC_DEFAULT}
+[ -n "\${BIGTOP_DEFAULTS_DIR}" -a -r \${BIGTOP_DEFAULTS_DIR}/hadoop ] && . \${BIGTOP_DEFAULTS_DIR}/hadoop
 
 # Autodetect JAVA_HOME if not defined
 if [ -e /usr/lib/bigtop-utils/bigtop-detect-javahome ]; then
@@ -226,30 +246,30 @@ if [ -e /usr/lib/bigtop-utils/bigtop-detect-javahome ]; then
 fi
 
 # FIXME: HCATALOG-636 (and also HIVE-2757)
-export HIVE_HOME=/usr/lib/hive
-export HIVE_CONF_DIR=/etc/hive/conf
-export HCAT_HOME=$INSTALLED_HCATALOG_DIR
+export HIVE_HOME=$HIVE_DIR
+export HIVE_CONF_DIR=$NP_ETC_HIVE/conf
+export HCAT_HOME=$HCATALOG_DIR
 
-export HCATALOG_HOME=$INSTALLED_HCATALOG_DIR
-exec $INSTALLED_HCATALOG_DIR/bin/hcat "\$@"
+export HCATALOG_HOME=$HCATALOG_DIR
+exec $HCATALOG_DIR/bin/hcat "\$@"
 EOF
 chmod 755 $wrapper
 
 # Install the docs
-install -d -m 0755 ${DOC_DIR}
-mv $HCATALOG_DIR/share/doc/hcatalog/* ${DOC_DIR}
+install -d -m 0755 $PREFIX/$DOC_DIR
+mv $PREFIX/$HCATALOG_DIR/share/doc/hcatalog/* $PREFIX/$DOC_DIR
 # Might as delete the directory since it's empty now
-rm -rf $HCATALOG_DIR/share/doc
-install -d -m 0755 $MAN_DIR
-gzip -c hive-hcatalog.1 > $MAN_DIR/hive-hcatalog.1.gz
+rm -rf $PREFIX/$HCATALOG_DIR/share/doc
+install -d -m 0755 $PREFIX/$MAN_DIR
+gzip -c hive-hcatalog.1 > $PREFIX/$MAN_DIR/hive-hcatalog.1.gz
 
 # Provide the runtime dirs
-install -d -m 0755 $PREFIX/var/lib/hive
+install -d -m 0755 $PREFIX/$VAR_HIVE_DIR
 install -d -m 0755 $PREFIX/var/log/hive
 
-install -d -m 0755 $PREFIX/var/lib/hive-hcatalog
+install -d -m 0755 $PREFIX/$VAR_HCATALOG_DIR
 install -d -m 0755 $PREFIX/var/log/hive-hcatalog
-for DIR in ${HCATALOG_SHARE_DIR} ; do
+for DIR in $PREFIX/$HCATALOG_SHARE_DIR ; do
     (cd $DIR &&
      for j in hive-hcatalog-*.jar; do
        if [[ $j =~ hive-hcatalog-(.*)-${HIVE_VERSION}.jar ]]; then
@@ -260,5 +280,5 @@ for DIR in ${HCATALOG_SHARE_DIR} ; do
 done
 
 # Remove Windows files
-find ${HIVE_DIR}/bin -name '*.cmd' | xargs rm -f
-find ${HCATALOG_DIR}/bin -name '*.cmd' | xargs rm -f
+find $PREFIX/$HIVE_DIR/bin -name '*.cmd' | xargs rm -f
+find $PREFIX/$HCATALOG_DIR/bin -name '*.cmd' | xargs rm -f
