61016895a22248ad33336801895ede02c05d7bc8
Yuqi Gu <yuqi.gu@arm.com>
2022-09-22 09:57:51 +0800

    BIGTOP-3812: Building bigtop-ambari-mpack fails on Debian/Ubuntu due to unwanted binary files (#1009)
    Building bigtop-ambari-mpack fails on Debian/Ubuntu due to unwanted binary files.
    We will move 'dev-support' to Bigtop provisioner and rename it to bigtop-stack-provisioner.
    It is used for Mpack provisioner which is the test tool for Mpack developers.
    After 3.2 release, we it will be used for Ambari UI test framework and Ambair-bigtop-demo.
    
    Change-Id: Ibe310d7ab979c055eacfc8e14daa27ca6aa415e3
    Signed-off-by: Yuqi Gu <yuqi.gu@arm.com>

--- provisioner/bigtop-stack-provisioner/docker/centos7/sync-scripts.sh
+++ provisioner/bigtop-stack-provisioner/docker/centos7/sync-scripts.sh
@@ -17,15 +17,15 @@
 
 echo -e "\033[32mSynchronizing script to ambari-server\033[0m"
 docker exec ambari-server bash -c "mkdir -p /var/lib/ambari-agent/cache/stacks/BGTP/1.0/services/"
-docker cp ../../../bgtp-ambari-mpack/src/main/resources/stacks/BGTP/1.0/services/ ambari-server:/var/lib/ambari-agent/cache/stacks/BGTP/1.0/
-docker cp ../../../bgtp-ambari-mpack/src/main/resources/stacks/BGTP/1.0/services/ ambari-server:/var/lib/ambari-server/resources/mpacks/bgtp-ambari-mpack-1.0.0.0-SNAPSHOT/stacks/BGTP/1.0/
+docker cp ../../../../bigtop-packages/src/common/bigtop-ambari-mpack/bgtp-ambari-mpack/src/main/resources/stacks/BGTP/1.0/services/ ambari-server:/var/lib/ambari-agent/cache/stacks/BGTP/1.0/
+docker cp ../../../../bigtop-packages/src/common/bigtop-ambari-mpack/bgtp-ambari-mpack/src/main/resources/stacks/BGTP/1.0/services/ ambari-server:/var/lib/ambari-server/resources/mpacks/bgtp-ambari-mpack-1.0.0.0-SNAPSHOT/stacks/BGTP/1.0/
 
 echo -e "\033[32mSynchronizing script to ambari-agent-01\033[0m"
 docker exec ambari-agent-01 bash -c "mkdir -p /var/lib/ambari-agent/cache/stacks/BGTP/1.0/services/"
-docker cp ../../../bgtp-ambari-mpack/src/main/resources/stacks/BGTP/1.0/services/ ambari-agent-01:/var/lib/ambari-agent/cache/stacks/BGTP/1.0/
+docker cp ../../../../bigtop-packages/src/common/bigtop-ambari-mpack/bgtp-ambari-mpack/src/main/resources/stacks/BGTP/1.0/services/ ambari-agent-01:/var/lib/ambari-agent/cache/stacks/BGTP/1.0/
 
 echo -e "\033[32mSynchronizing script to ambari-agent-02\033[0m"
 docker exec ambari-agent-02 bash -c "mkdir -p /var/lib/ambari-agent/cache/stacks/BGTP/1.0/services/"
-docker cp ../../../bgtp-ambari-mpack/src/main/resources/stacks/BGTP/1.0/services/ ambari-agent-02:/var/lib/ambari-agent/cache/stacks/BGTP/1.0/
+docker cp ../../../../bigtop-packages/src/common/bigtop-ambari-mpack/bgtp-ambari-mpack/src/main/resources/stacks/BGTP/1.0/services/ ambari-agent-02:/var/lib/ambari-agent/cache/stacks/BGTP/1.0/
 
 echo -e "\033[32mDone!\033[0m"
