cb24e6a1e021e51fea1ee6b684a0b69b9cfc7192
timyuer <524860213@qq.com>
2022-10-15 13:51:04 +0800

    BIGTOP-3847: Set bigtop-select to be consistent with -PparentDir (#1038)

--- bigtop-packages/src/common/bigtop-select/install_select.sh
+++ bigtop-packages/src/common/bigtop-select/install_select.sh
@@ -21,9 +21,11 @@ usage() {
   echo "
 usage: $0 <options>
   Required not-so-options:
-     --distro-dir=DIR            path to distro specific files (debian/RPM)
-     --build-dir=DIR             path to build directory
-     --prefix=PREFIX             path to install into
+     --distro-dir=DIR                             path to distro specific files (debian/RPM)
+     --build-dir=DIR                              path to build directory
+     --prefix=PREFIX                              path to install into
+     --parent-dir=PARENT_DIR                      path to install stack-root/stack-version
+     --bigtop-base-version=BIGTOP_BASE_VERSION    Bigtop Base Version
   "
   exit 1
 }
@@ -33,6 +35,8 @@ OPTS=$(getopt \
   -l 'prefix:' \
   -l 'distro-dir:' \
   -l 'build-dir:' \
+  -l 'parent-dir:' \
+  -l 'bigtop-base-version:' \
   -- "$@")
 if [ $? != 0 ] ; then
     usage
@@ -40,6 +44,12 @@ fi
 eval set -- "$OPTS"
 while true ; do
     case "$1" in
+        --parent-dir)
+        PARENT_DIR=$2 ; shift 2
+        ;;
+        --bigtop-base-version)
+        BIGTOP_BASE_VERSION=$2 ; shift 2
+        ;;
         --prefix)
         PREFIX=$2 ; shift 2
         ;;
@@ -67,12 +77,37 @@ LIB_DIR=${LIB_DIR:-/usr/lib/bigtop-select}
 BIN_DIR=${BIN_DIR:-/usr/bin}
 CONF_DIR=${CONF_DIR:-/etc/bigtop-select/conf.dist}
 
-STACK_ROOT_DIR=/usr/bigtop
+STACK_ROOT_DIR=$(echo ${PARENT_DIR} | sed -e "s/\/${BIGTOP_BASE_VERSION}$//")
 STACK_SELECTOR=distro-select
 CONF_SELECTOR=conf-select
 
 # Install packages
 install -d -p -m 755 $PREFIX${LIB_DIR}/
-install -d -p -m 755 $PREFIX${STACK_ROOT_DIR}/
+install -d -p -m 755 $PREFIX${PARENT_DIR}/
 install -p -m 755 ${DISTRO_DIR}/${STACK_SELECTOR} $PREFIX${LIB_DIR}/
 install -p -m 755 ${DISTRO_DIR}/${CONF_SELECTOR} $PREFIX${LIB_DIR}/
+cat > $PREFIX${LIB_DIR}/params.py <<EOF
+#!/usr/bin/env python2
+#
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+stack_root = '${STACK_ROOT_DIR}'
+stack_version = '${BIGTOP_BASE_VERSION}'
+
+EOF
+chmod 755 $PREFIX${LIB_DIR}/params.py
--- bigtop-packages/src/rpm/bigtop-select/SPECS/bigtop-select.spec
+++ bigtop-packages/src/rpm/bigtop-select/SPECS/bigtop-select.spec
@@ -13,9 +13,12 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-%define lib_dir        /usr/lib/bigtop-select
-%define bin_dir        /usr/bin
-%define stack_root_dir /usr/bigtop
+%define lib_dir              /usr/lib/bigtop-select
+%define bin_dir              /usr/bin
+%define default_parent_dir   /usr/bigtop/%{bigtop_base_version}
+%if "%{parent_dir}" != ""
+%define default_parent_dir   %{parent_dir}
+%endif
 
 Name: bigtop-select
 Version: %{bigtop_select_version}
@@ -52,6 +55,7 @@ install -p -m 755 %{SOURCE1} .
 install -p -m 755 %{SOURCE2} .
 install -p -m 755 %{SOURCE3} .
 
+
 %build
 
 
@@ -59,7 +63,9 @@ install -p -m 755 %{SOURCE3} .
 bash %{SOURCE0} \
   --distro-dir=${RPM_SOURCE_DIR} \
   --build-dir=${PWD} \
-  --prefix=${RPM_BUILD_ROOT}
+  --prefix=${RPM_BUILD_ROOT} \
+  --parent-dir=%{default_parent_dir} \
+  --bigtop-base-version=%{bigtop_base_version}
 
 %clean
 rm -rf $RPM_BUILD_ROOT
@@ -70,7 +76,7 @@ rm -rf $RPM_BUILD_ROOT
 %doc LICENSE
 
 %{lib_dir}
-%{stack_root_dir}
+%{default_parent_dir}
 
 %changelog
 
