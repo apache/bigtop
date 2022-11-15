ab952ed2bb25b015b416da149cf6808d839bfceb
MacChen <58972037+MacChen02@users.noreply.github.com>
2022-11-15 09:45:55 +0800

    Update gnupg.pp

--- bigtop_toolchain/manifests/gnupg.pp
+++ bigtop_toolchain/manifests/gnupg.pp
@@ -16,7 +16,7 @@
 class bigtop_toolchain::gnupg {
 
   case $operatingsystem {
-    /(?i:(centos|fedora|redhat))/: {
+    /(?i:(centos|fedora|redhat|openEuler))/: {
        $pkg = "gnupg2"
        $cmd = "gpg2"
     }
