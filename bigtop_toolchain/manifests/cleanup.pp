10fb152d32844030a78c97187977571f8197ecb1
MacChen <58972037+MacChen02@users.noreply.github.com>
2022-11-15 14:40:52 +0800

    Update cleanup.pp

--- bigtop_toolchain/manifests/cleanup.pp
+++ bigtop_toolchain/manifests/cleanup.pp
@@ -15,7 +15,7 @@
 
 class bigtop_toolchain::cleanup {
   $packager_cleanup = $operatingsystem ? {
-    /(?i:(centos|fedora|redhat|amazon))/ => 'yum clean all',
+    /(?i:(centos|fedora|redhat|amazon|openEuler))/ => 'yum clean all',
     /(?i:(SLES|opensuse))/ => 'zypper clean -a',
     /Ubuntu|Debian/        => 'apt-get clean',
   } 
