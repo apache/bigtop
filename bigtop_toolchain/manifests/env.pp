1aa40d0fb91e37e7acc604c282d72309cd4fb9c4
MacChen <58972037+MacChen02@users.noreply.github.com>
2022-11-15 09:45:23 +0800

    Update env.pp

--- bigtop_toolchain/manifests/env.pp
+++ bigtop_toolchain/manifests/env.pp
@@ -35,7 +35,7 @@ class bigtop_toolchain::env {
     'Ubuntu': {
       $javahome = "/usr/lib/jvm/${java}-openjdk-$arch"
     }
-    'Fedora','Centos','RedHat','Amazon': {
+    'Fedora','Centos','RedHat','Amazon','openEuler': {
       $javahome = "/usr/lib/jvm/${java}"
     }
     'OpenSuSE' : {
