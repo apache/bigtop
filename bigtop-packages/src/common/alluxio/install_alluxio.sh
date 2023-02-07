1acbb9e2e2ba6c06b7e0898ed3c299affe2dfe8b
MacChen <58972037+MacChen02@users.noreply.github.com>
2023-02-07 14:49:51 +0800

    refresh the format of space

--- bigtop-packages/src/common/alluxio/install_alluxio.sh
+++ bigtop-packages/src/common/alluxio/install_alluxio.sh
@@ -132,7 +132,7 @@ cp -a libexec/* $PREFIX/$LIB_DIR/libexec
 cp -a client/* $PREFIX/$LIB_DIR/client
 cp -a integration/* $PREFIX/$LIB_DIR/integration
 cp integration/fuse/target/alluxio-integration-fuse-*-jar-with-dependencies.jar $PREFIX/$LIB_DIR/integration/fuse
-if [${OS} = "openEuler"] ; then
+if [ ${OS} = "openEuler" ]; then
    cp integration/jnifuse/native/src/main/resources/libjnifuse*.so $PREFIX/$LIB_DIR/integration/jnifuse/native/target/classes/
 fi
 rm -rf $PREFIX/$LIB_DIR/integration/pom.xml $PREFIX/$LIB_DIR/integration/**/pom.xml
