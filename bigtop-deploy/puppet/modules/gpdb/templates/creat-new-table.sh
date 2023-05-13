8ad48bd62d7dc957ec58c7cffc65b220e81abe37
Yuqi Gu <yuqi.gu@arm.com>
2023-05-13 05:26:27 +0000

    Changed the solution of rpath check for avoiding additional LD_LIBRARY_PATH setting
    Change-Id: I8e9d219a2c4a23356ae4fde467fde749eef764ff
    Signed-off-by: Yuqi Gu <yuqi.gu@arm.com>

--- bigtop-deploy/puppet/modules/gpdb/templates/creat-new-table.sh
+++ bigtop-deploy/puppet/modules/gpdb/templates/creat-new-table.sh
@@ -1,11 +1,2 @@
 #!/bin/sh
-if [ -f /etc/os-release ]; then
-    . /etc/os-release
-fi
-OS="$ID"
-
-if [ "${OS}" = "fedora" ]; then
-    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/gpdb/lib;
-fi
-
 export PGOPTIONS="-c gp_session_role=utility";<%= @gp_home %>/bin/psql -p <%= @master_port %> -d "template1" -c "CREATE TABLE bigtopusers (id integer, name text, age numeric);" || true
--- bigtop-deploy/puppet/modules/gpdb/templates/init-db.sh
+++ bigtop-deploy/puppet/modules/gpdb/templates/init-db.sh
@@ -1,13 +1,4 @@
 #!/bin/sh
-if [ -f /etc/os-release ]; then
-    . /etc/os-release
-fi
-OS="$ID"
-
-if [ "${OS}" = "fedora" ]; then
-    export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/lib/gpdb/lib;
-fi
-
 if [ ! -d $1 ]; then
   export LD_LIBRARY_PATH=<%= @gp_home %>/lib:/lib;<%= @gp_home %>/bin/initdb -E UNICODE -D $1 --max_connections=750 --shared_buffers=128000kB --backend_output=$1.initdb
 fi
