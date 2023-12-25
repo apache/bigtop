90470f7cfa28062c0b025fba1c04337c9e9c4603
vivostar <lishuaipeng651@gmail.com>
2023-12-25 17:25:43 +0800

    BIGTOP-3979. Drop ubuntu-20.04 support

--- bigtop_toolchain/bin/puppetize.sh
+++ bigtop_toolchain/bin/puppetize.sh
@@ -30,7 +30,7 @@ case ${ID}-${VERSION_ID} in
         # So we install that module in the same way as CentOS 7.
         puppet module install puppetlabs-stdlib --version 4.12.0
         ;;
-    ubuntu-18.04|ubuntu-20.04|ubuntu-22.04)
+    ubuntu-18.04|ubuntu-22.04)
         apt-get update
         apt-get -y install wget curl sudo unzip puppet software-properties-common puppet-module-puppetlabs-apt puppet-module-puppetlabs-stdlib systemd-sysv
         ;;
