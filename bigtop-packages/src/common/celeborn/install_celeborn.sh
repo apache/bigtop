7e6ce8c85f0dec9d94b923878c11296243ab589f
jialiang <jialiangcaimd@gmail.com>
2024-01-30 16:06:54 +0800

    fix celeborn package

--- bigtop-packages/src/common/celeborn/install_celeborn.sh
+++ bigtop-packages/src/common/celeborn/install_celeborn.sh
@@ -20,19 +20,13 @@ usage() {
   echo "
 usage: $0 <options>
   Required not-so-options:
-     --build-dir=DIR             path to celeborn
-      dist.dir
+     --build-dir=DIR             path to celeborn dist.dir
      --prefix=PREFIX             path to install into
 
   Optional options:
-     --lib-dir=DIR               path to install celeborn
-      home [/usr/lib/celeborn
-      ]
+     --lib-dir=DIR               path to install celeborn home [/usr/lib/celeborn]
      --bin-dir=DIR               path to install bins [/usr/bin]
-     --etc-celeborn
-     =DIR             path to install celeborn
-      conf [/etc/celeborn
-      ]
+     --etc-celeborn=DIR          path to install celeborn conf [/etc/celeborn]
      ... [ see source for more similar options ]
   "
   exit 1
@@ -45,8 +39,7 @@ OPTS=$(getopt \
   -l 'build-dir:' \
   -l 'bin-dir:' \
   -l 'lib-dir:' \
-  -l 'etc-celeborn
-  :' -- "$@")
+  -l 'etc-celeborn:' -- "$@")
 
 if [ $? != 0 ] ; then
     usage
@@ -67,8 +60,7 @@ while true ; do
         --lib-dir)
         LIB_DIR=$2 ; shift 2
         ;;
-        --etc-celeborn
-        )
+        --etc-celeborn)
         ETC_CELEBORN=$2 ; shift 2
         ;;
         --)
@@ -89,20 +81,14 @@ for var in PREFIX BUILD_DIR ; do
   fi
 done
 
-LIB_DIR=${LIB_DIR:-/usr/lib/celeborn
-}
+LIB_DIR=${LIB_DIR:-/usr/lib/celeborn}
 BIN_DIR=${BIN_DIR:-/usr/bin}
-ETC_CELEBORN=${ETC_CELEBORN:-/etc/celeborn
-}
-RUN_DIR=${RUN_DIR:-/var/run/celeborn
-}
-LOG_DIR=${LOG_DIR:-/var/log/celeborn
-}
+ETC_CELEBORN=${ETC_CELEBORN:-/etc/celeborn}
+RUN_DIR=${RUN_DIR:-/var/run/celeborn}
+LOG_DIR=${LOG_DIR:-/var/log/celeborn}
 
 NP_ETC_CELEBORN=/etc/celeborn
 
-NP_VAR_LIB_CELEBORN_DATA=/var/lib/celeborn
-/data
 
 
 install -d -m 0755 $PREFIX/$LIB_DIR
@@ -124,20 +110,20 @@ install -d -m 0755 $PREFIX/$LOG_DIR
 
 TMP_DIR=$BUILD_DIR/tmp
 mkdir -p $BUILD_DIR/tmp
-tar -zxf $BUILD_DIR/build/celeborn-release*.tar.gz -C $TMP_DIR
+tar -zxf $BUILD_DIR/apache-celeborn-*-bin.tgz -C $TMP_DIR
 
-cp -ra ${TMP_DIR}/celeborn-*/bin/* ${PREFIX}/${LIB_DIR}/bin/
-cp -ra ${TMP_DIR}/celeborn-*/charts/* ${PREFIX}/${LIB_DIR}/charts/
-cp -ra ${TMP_DIR}/celeborn-*/docker/* ${PREFIX}/${LIB_DIR}/docker/
-cp -ra ${TMP_DIR}/celeborn-*/flink/* ${PREFIX}/${LIB_DIR}/flink/
-cp -ra ${TMP_DIR}/celeborn-*/jars/* ${PREFIX}/${LIB_DIR}/jars/
-cp -ra ${TMP_DIR}/celeborn-*/master-jars/* ${PREFIX}/${LIB_DIR}/master-jars/
-cp -ra ${TMP_DIR}/celeborn-*/mr/* ${PREFIX}/${LIB_DIR}/mr/
-cp -ra ${TMP_DIR}/celeborn-*/sbin/* ${PREFIX}/${LIB_DIR}/sbin/
-cp -ra ${TMP_DIR}/celeborn-*/spark/* ${PREFIX}/${LIB_DIR}/spark/
-cp -ra ${TMP_DIR}/celeborn-*/worker-jars/* ${PREFIX}/${LIB_DIR}/worker-jars/
+cp -ra ${TMP_DIR}/apache-celeborn-*-bin/bin/* ${PREFIX}/${LIB_DIR}/bin/
+cp -ra ${TMP_DIR}/apache-celeborn-*-bin/charts/* ${PREFIX}/${LIB_DIR}/charts/
+cp -ra ${TMP_DIR}/apache-celeborn-*-bin/docker/* ${PREFIX}/${LIB_DIR}/docker/
+cp -ra ${TMP_DIR}/apache-celeborn-*-bin/flink/* ${PREFIX}/${LIB_DIR}/flink/
+cp -ra ${TMP_DIR}/apache-celeborn-*-bin/jars/* ${PREFIX}/${LIB_DIR}/jars/
+cp -ra ${TMP_DIR}/apache-celeborn-*-bin/master-jars/* ${PREFIX}/${LIB_DIR}/master-jars/
+cp -ra ${TMP_DIR}/apache-celeborn-*-bin/mr/* ${PREFIX}/${LIB_DIR}/mr/
+cp -ra ${TMP_DIR}/apache-celeborn-*-bin/sbin/* ${PREFIX}/${LIB_DIR}/sbin/
+cp -ra ${TMP_DIR}/apache-celeborn-*-bin/spark/* ${PREFIX}/${LIB_DIR}/spark/
+cp -ra ${TMP_DIR}/apache-celeborn-*-bin/worker-jars/* ${PREFIX}/${LIB_DIR}/worker-jars/
 
-cp -ra ${TMP_DIR}/celeborn-*/conf/* $PREFIX/$ETC_CELEBORN/conf.dist/
+cp -ra ${TMP_DIR}/apache-celeborn-*-bin/conf/* $PREFIX/$ETC_CELEBORN/conf.dist/
 
 
 ln -s $NP_ETC_CELEBORN/conf $PREFIX/$LIB_DIR/conf
