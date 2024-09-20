9fb0de3df5e81d50f4cfbe70bf09985eafb0e2e2
Shuhei Yamasaki <yamasakisua@oss.nttdata.com>
2024-09-20 15:30:10 +0900

    BIGTOP-4157. Drop CentOS 7 (#1296)

--- docker/bigtop-slaves/build.sh
+++ docker/bigtop-slaves/build.sh
@@ -22,8 +22,8 @@ if [ $# != 1 ]; then
   echo
   echo "Usage: build.sh <PREFIX-OS-VERSION>"
   echo
-  echo "Example: build.sh trunk-centos-7"
-  echo "       : build.sh 1.0.0-centos-7"
+  echo "Example: build.sh trunk-ubuntu-22.04"
+  echo "       : build.sh 1.0.0-ubuntu-22.04"
   exit 1
 fi
 
