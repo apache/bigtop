e307d8254a927670af1b9bf5c904f18568e92c01
Masatake Iwasaki <iwasakims@apache.org>
2025-03-04 09:39:57 +0900

    BIGTOP-4382. Fix error on updating java alternatives on fedora-40. (#1335)
    (cherry picked from commit d9b64f7d4dd954c29668a6903180c82b63c2ad60)

--- bigtop_toolchain/manifests/jdk.pp
+++ bigtop_toolchain/manifests/jdk.pp
@@ -18,21 +18,25 @@ class bigtop_toolchain::jdk {
     /Debian/: {
       include apt
 
-      package { 'temurin-8-jdk' :
+      package { 'jdk8' :
+        name => 'temurin-8-jdk',
         ensure => present,
       }
     }
     /Ubuntu/: {
       include apt
 
-      package { 'openjdk-8-jdk' :
+      package { 'jdk8' :
+        name => 'openjdk-8-jdk',
         ensure  => present,
       }
     }
     /(CentOS|Amazon|Fedora|RedHat|Rocky|openEuler)/: {
-      package { 'java-1.8.0-openjdk-devel' :
+      package { 'jdk8' :
+        name => 'java-1.8.0-openjdk-devel',
         ensure => present
       }
+
       if ($::operatingsystem == "Fedora") {
         file { '/usr/lib/jvm/java-1.8.0-openjdk/jre/lib/security/cacerts':
           ensure => 'link',
@@ -41,8 +45,42 @@ class bigtop_toolchain::jdk {
       }
     }
     /OpenSuSE/: {
-      package { 'java-1_8_0-openjdk-devel' :
+      package { 'jdk8' :
+        name => 'java-1_8_0-openjdk-devel',
         ensure => present
+      }
+    }
+  }
+
+  case $::operatingsystem {
+    /Debian/: {
+      exec { 'ensure java 8 is set as default':
+        command => "update-java-alternatives --set temurin-8*",
+        path    => ['/usr/sbin', '/usr/bin', '/bin'],
+        require => Package['jdk8'],
+      }
+    }
+    /Ubuntu/: {
+      exec { 'ensure java 8 is set as default':
+        command => "update-java-alternatives --set java-1.8.0-openjdk-$(dpkg --print-architecture)",
+        path    => ['/usr/sbin', '/usr/bin', '/bin'],
+        require => Package['jdk8'],
+      }
+    }
+    /(CentOS|RedHat|Rocky)/: {
+      exec { 'ensure java 8 is set as default':
+        command => "update-alternatives --set java java-1.8.0-openjdk.$(uname -m) \
+                    && update-alternatives --set javac java-1.8.0-openjdk.$(uname -m)",
+        path    => ['/usr/sbin', '/usr/bin', '/bin'],
+        require => Package['jdk8'],
+      }
+    }
+    /Fedora/: {
+      exec { 'ensure java 8 is set as default':
+        command => "update-alternatives --set java /usr/lib/jvm/java-1.8.0-openjdk/jre/bin/java \
+                    && update-alternatives --set javac /usr/lib/jvm/java-1.8.0-openjdk/bin/javac",
+        path    => ['/usr/sbin', '/usr/bin', '/bin'],
+        require => Package['jdk8'],
       }
     }
   }
