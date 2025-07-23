4c6574131728653df9a9981a0392c17ee71dab7f
Kengo Seki <sekikn@apache.org>
2025-07-23 15:27:13 +0900

    BIGTOP-4471. Upgrade the Puppet manifest for Airflow to adopt PostgreSQL and LocalExecutor

--- bigtop-deploy/puppet/modules/airflow/manifests/init.pp
+++ bigtop-deploy/puppet/modules/airflow/manifests/init.pp
@@ -20,7 +20,11 @@ class airflow {
     }
   }
 
-  class server($executor, $load_examples, $sql_alchemy_conn, $install_bigpetstore_example=False) {
+  class server($executor="SequentialExecutor",
+    $load_examples=true,
+    $sql_alchemy_conn="sqlite:////var/lib/airflow/airflow.db",
+    $install_bigpetstore_example=false,
+  ) {
     package { 'airflow':
       ensure => latest,
     }
@@ -32,11 +36,83 @@ class airflow {
       require => Package['airflow'],
     }
 
-    exec { 'airflow-db-init':
-      command     => '/usr/lib/airflow/bin/airflow db init',
-      environment => ['AIRFLOW_HOME=/var/lib/airflow'],
-      user        => 'airflow',
-      require     => File['/var/lib/airflow/airflow.cfg'],
+    # The built-in scanf function (https://www.puppet.com/docs/puppet/7/function.html#scanf) doesn't work
+    # on Debian and Ubuntu for some reason, so we define a function for parsing SQLAlchemy's database URL
+    # (https://docs.sqlalchemy.org/en/20/core/engines.html#database-urls) ourselves here.
+    $parsed_url = parse_database_url($sql_alchemy_conn)
+    $dialect = $parsed_url[0]
+    $username = $parsed_url[1]
+    $password = $parsed_url[2]
+    $database = $parsed_url[3]
+
+    if $dialect =~ /^postgres/ {
+      # Install Airflow's Postgres Provider
+      exec { 'install-postgres-provider':
+        command     => ['/usr/lib/airflow/bin/python3', '-m', 'pip', 'install', 'apache-airflow-providers-postgres'],
+        environment => ['AIRFLOW_HOME=/var/lib/airflow'],
+        require     => File['/var/lib/airflow/airflow.cfg'],
+        user        => 'root',
+      }
+
+      # Install and enable PostgreSQL
+      if $operatingsystem =~ /^(?i:(ubuntu|debian))$/ {
+        package { 'postgresql':
+          ensure => latest,
+        }
+        service { 'postgresql':
+          ensure  => running,
+          require => Package['postgresql'],
+        }
+      } else {
+        package { 'postgresql-server':
+          ensure => latest
+        }
+        exec { 'initdb':
+          command => '/usr/bin/pg_ctl initdb -D /var/lib/pgsql/data',
+          user    => 'postgres',
+          require => Package['postgresql-server']
+        }
+        service { 'postgresql':
+          ensure  => running,
+          require => Exec['initdb']
+        }
+      }
+
+      # Set up Airflow's database backend in accordance with
+      # https://airflow.apache.org/docs/apache-airflow/2.10.5/howto/set-up-database.html#setting-up-a-postgresql-database
+      exec { 'create-airflow-database':
+        command => ["/usr/bin/psql", "-c", "CREATE DATABASE ${database}"],
+        user    => 'postgres',
+        require => Service['postgresql']
+      }
+      exec { 'create-airflow-user':
+        command => ["/usr/bin/psql", "-c", "CREATE USER ${username} WITH PASSWORD '${password}'"],
+        user    => 'postgres',
+        require => Exec['create-airflow-database']
+      }
+      exec { 'grant-airflow-privileges':
+        command => ["/usr/bin/psql", "-c", "GRANT ALL PRIVILEGES ON DATABASE ${database} TO ${username}"],
+        user    => 'postgres',
+        require => Exec['create-airflow-user'],
+      }
+      exec { 'alter-database-owner':
+        command => ["/usr/bin/psql", "-c", "ALTER DATABASE ${database} OWNER TO ${username}"],
+        user    => 'postgres',
+        require => Exec['grant-airflow-privileges'],
+      }
+      exec { 'airflow-db-init':
+        command     => '/usr/lib/airflow/bin/airflow db init',
+        environment => ['AIRFLOW_HOME=/var/lib/airflow'],
+        user        => 'airflow',
+        require     => Exec['alter-database-owner'],
+      }
+    } else {
+      exec { 'airflow-db-init':
+        command     => '/usr/lib/airflow/bin/airflow db init',
+        environment => ['AIRFLOW_HOME=/var/lib/airflow'],
+        user        => 'airflow',
+        require     => File['/var/lib/airflow/airflow.cfg'],
+      }
     }
 
     exec { 'airflow-users-create':
